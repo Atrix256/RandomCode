0ffaf106de8cd98048b5bd8be7bf10ddd28120b5
Atrix256 <alan.wolfe@gmail.com>
2015-09-18 20:56:35 -0700

    Time to multithread it

--- BruteForce/Source.cpp
+++ BruteForce/Source.cpp
@@ -6,7 +6,7 @@ typedef int TInteger;
 const size_t c_numInputs = 2;
 const size_t c_numKeys = 1 << c_numInputs;
 
-const TInteger c_maxSearchValue = 2;
+const TInteger c_maxSearchValue = 10;
 
 //=================================================================================
 void WaitForEnter ()
@@ -57,7 +57,15 @@ bool InputSatisfiesConstraints (
     const std::array<TInteger, c_numKeys>& keys
 )
 {
-    // TODO: make this work!
+    for (size_t input = 0; input < c_numInputs; ++input)
+    {
+        for (size_t key = 0; key < c_numKeys; ++key)
+        {
+            int output = (key & (1 << input)) != 0;
+            if ((inputs[input] % keys[key]) % 2 != output)
+                return false;
+        }
+    }
     return true;
 }
 
@@ -115,52 +123,28 @@ void ProcessKeySpace (TInteger keySpace)
 }
 
 //=================================================================================
+void PrintEquations ()
+{
+    for (size_t input = 0; input < c_numInputs; ++input)
+    {
+        for (size_t key = 0; key < c_numKeys; ++key)
+        {
+            int output = (key & (1 << input)) != 0;
+            printf("(x%i %% k%i) %% 2 = %i\n", input, key, output);
+        }
+        printf("\n");
+    }
+}
+
+//=================================================================================
 int main (int argc, char **argv)
 {
-
-    for (TInteger keySpace = 1; keySpace < c_maxSearchValue; ++keySpace)
+    PrintEquations();
+    for (TInteger keySpace = 1; keySpace <= c_maxSearchValue; ++keySpace)
     {
         printf("%i / %i\n", keySpace, c_maxSearchValue);
         ProcessKeySpace(keySpace);
     }
-
-#if 0
-    int x0, x1, k0, k1, k2, k3;
-
-    const int c_maxAmount = 100;
-
-    for (x0 = 0; x0 < c_maxAmount; ++x0)
-    {
-        printf("%i / %i\n", x0, c_maxAmount);
-        for (x1 = 0; x1 < c_maxAmount; ++x1)
-            for (k0 = 1; k0 < c_maxAmount; ++k0)
-                for (k1 = 1; k1 < c_maxAmount; ++k1)
-                    for (k2 = 1; k2 < c_maxAmount; ++k2)
-                        for (k3 = 1; k3 < c_maxAmount; ++k3)
-                        {
-                            if (((x0 % k0) % 2) == 0 &&
-                                ((x0 % k1) % 2) == 1 &&
-                                ((x0 % k2) % 2) == 0 &&
-                                ((x0 % k3) % 2) == 1 &&
-                                ((x1 % k0) % 2) == 0 &&
-                                ((x1 % k1) % 2) == 0 &&
-                                ((x1 % k2) % 2) == 1 &&
-                                ((x1 % k3) % 2) == 1
-                                /*
-                                &&
-                                (k0 % 2) == 0 &&
-                                (k1 % 2) == 0 && 
-                                (k2 % 2) == 0 && 
-                                (k3 % 2) == 0
-                                */
-                                )
-                            {
-                                printf("%i, %i, %i, %i, %i, %i\n", x0, x1, k0, k1, k2, k3);
-                                WaitForEnter();
-                            }
-                        }
-    }
-#endif
 
     WaitForEnter();
     return 0;
@@ -168,21 +152,22 @@ int main (int argc, char **argv)
 
 /*
 TODO:
+* multithread it
 ? are there any where the keys are all even
  * probably not. I let it run up to 40, for max number = 100 and it didnt find any
 * test this in homomorphic setting (n bit adder?)
 * generalize program for N bits and whatever highest number search
-* multithread it
 * try and figure it out mathematically
 * after proving that this stuff works homomorphically, gather up what needs to be done, and possible avenues to explore, and present to ben to see if he wants to collaborate.
 * make a private github and share w/ him if he wants in
-* make it threaded?
 
 Paper Research TODO's
 * find math to solve the constraints.  brute force is slow as f00k
  * look into successive substitution to solve these equations? https://en.wikipedia.org/wiki/Method_of_successive_substitution
 * is there a way to have even keys? i don't think so but...
 * how about the other way(s) to implement homomorphic encryption.  Does this apply there? are any more interesting / easily done or have better properties?
+? will modulus switching work?
+? maybe bootstrapping would be better
 
 
 FIRST ANSWER:
