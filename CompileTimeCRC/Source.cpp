127dfa295932b8acb2f14fbf37081ea5d335fd61
Atrix256 <alan.wolfe@gmail.com>
2016-09-23 13:44:01 -0700

    All done with CompileTimeCRC

--- CompileTimeCRC/Source.cpp
+++ CompileTimeCRC/Source.cpp
@@ -7,32 +7,31 @@ constexpr unsigned int crc32(const char *s, unsigned int salt = 0) {
 }
 
 void Snippet_CompileTimeHashing (void) {
-
     const char *hello1String = "Hello1";
-    unsigned int hashHello1 = crc32(hello1String);  // x86x/x64 - Debug/Release: Run Time.
-    unsigned int hashHello2 = crc32("Hello2");      // x86x/x64 - Debug/Release: Run Time.
+    unsigned int hashHello1 = crc32(hello1String);  // 1) Always Run Time.
+    unsigned int hashHello2 = crc32("Hello2");      // 2) Always Run Time.
 
-    const char *hello3String = "Hello3";
-    // error C2131: expression did not evaluate to a constant
+    // 3) error C2131: expression did not evaluate to a constant
+    //const char *hello3String = "Hello3";
     //constexpr unsigned int hashHello3 = crc32(hello3String);
-    constexpr unsigned int hashHello4 = crc32("Hello4");  // x86x/x64 - Debug: Run Time.  Release: Compile Time
+    constexpr unsigned int hashHello4 = crc32("Hello4");  // 4) Debug: Run Time.  Release: Compile Time
 
-    printf("%X %X %X %X\n", hashHello1, hashHello2, hashHello4, crc32("hello5"));  // x86x/x64 - Debug/Release: Run Time. (!!!)
+    printf("%X %X %X %X\n", hashHello1, hashHello2, hashHello4, crc32("hello5"));  // 5) Always Run Time. (!!!)
 }
 
 void Snippet_CompileTimeHashSwitching1 (void) {
-    unsigned int hash = crc32("Hello1");  // Run Time.
-    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // Debug: Run Time. Release: Not calculated at all.
-    switch (hash) { // Uses variable on stack
-        case hashTestHello2: {  // Compile Time Constant.
+    unsigned int hash = crc32("Hello1");  // 1) Run Time.
+    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // 2) Debug: Run Time. Release: Not calculated at all.
+    switch (hash) { // 3) Uses variable on stack
+        case hashTestHello2: {  // 4) Compile Time Constant.
             printf("A\n");
             break;
         }
-        case crc32("Hello3"): {  // Compile Time Constant.
+        case crc32("Hello3"): {  // 5) Compile Time Constant.
             printf("B\n");
             break;
         }
-        // error C2196: case value '1470747604' already used
+        // 6) error C2196: case value '1470747604' already used
         /*
         case crc32("Hello2"): { 
             printf("C\n");
@@ -48,15 +47,14 @@ void Snippet_CompileTimeHashSwitching1 (void) {
 
 void Snippet_CompileTimeHashSwitching2 (void) {
     // Release: this function just prints "C\n" and exits.  All code melted away at compile time!
-
-    constexpr unsigned int hash = crc32("Hello1");  // Debug: Run Time
-    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // Debug: Run Time
-    switch (hash) { // Debug: Compile Time Constant.
-        case hashTestHello2: {   // Debug: Compile Time Constant.
+    constexpr unsigned int hash = crc32("Hello1");  // 1) Debug: Run Time
+    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // 2) Debug: Run Time
+    switch (hash) { // 3) Debug: Compile Time Constant.
+        case hashTestHello2: {   // 4) Debug: Compile Time Constant.
             printf("A\n");
             break;
         }
-        case crc32("Hello3"): {   // Debug: Compile Time Constant.
+        case crc32("Hello3"): {   // 5) Debug: Compile Time Constant.
             printf("B\n");
             break;
         }
@@ -68,13 +66,13 @@ void Snippet_CompileTimeHashSwitching2 (void) {
 }
 
 void Snippet_CompileTimeHashSwitching3 (void) {
-    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // Debug: Run Time. Release: Not calculated at all.
-    switch (crc32("Hello1")) { // Always Run Time (!!!)
-        case hashTestHello2: {  // Compile Time Constant.
+    constexpr unsigned int hashTestHello2 = crc32("Hello2"); // 1) Debug: Run Time. Release: Not calculated at all.
+    switch (crc32("Hello1")) {  // 2) Always Run Time (!!!)
+        case hashTestHello2: {  // 3) Compile Time Constant.
             printf("A\n");
             break;
         }
-        case crc32("Hello3"): {  // Compile Time Constant.
+        case crc32("Hello3"): {  // 4) Compile Time Constant.
             printf("B\n");
             break;
         }
@@ -160,38 +158,39 @@ void Snippet_CompileTimePerfectHashing () {
 void Snippet_FindMinimalPerfectHashingSalt ()
 {
     // This takes a long time and ends up not finding any salt value that prevents collisions!
+    static const unsigned int c_numBuckets = 8;
     for (unsigned int salt = 1; salt != 0; ++salt) {
         unsigned char mask = 0;
         unsigned int newValue;
-        newValue = 1 << (crc32("Identifier_A", salt) % 8);
+        newValue = 1 << (crc32("Identifier_A", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_B", salt) % 8);
+        newValue = 1 << (crc32("Identifier_B", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_C", salt) % 8);
+        newValue = 1 << (crc32("Identifier_C", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_D", salt) % 8);
+        newValue = 1 << (crc32("Identifier_D", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_E", salt) % 8);
+        newValue = 1 << (crc32("Identifier_E", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_F", salt) % 8);
+        newValue = 1 << (crc32("Identifier_F", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_G", salt) % 8);
+        newValue = 1 << (crc32("Identifier_G", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
-        newValue = 1 << (crc32("Identifier_H", salt) % 8);
+        newValue = 1 << (crc32("Identifier_H", salt) % c_numBuckets);
         if (newValue&mask)
             continue;
         mask |= newValue;
@@ -232,51 +231,56 @@ void Snippet_CompileTimeMinimalPerfectHash ()
     printf("id = %i\n", id);
 }
 
-void Snippet_CompileTimeAssistedStringToEnum ()
+void Snippet_CompileTimeHashAssistedStringToEnum ()
 {
     // Debug / Release:
     //   Runs crc32 at runtime only for "i".  The cases are compile time constants as per usual.
     //   Does a jumptable type setup for the switch and does a string comparison against the correct string.
     //   If strings are equal, sets the enum value.
     //
-    // release when making it constexpr:
-    //  Does string compare, and if strings are equal, sets the enum value. (!!!)
-    //  Strangely, if i change the strings to "Identifier_FZ" it doesn't do the string compare.
-    //  Maybe because it's the wrong length?? Not really sure... (!!!)
     static const unsigned int c_numBuckets = 16;
     static const unsigned int c_salt = 1337;
 
     const char* testString = "Identifier_F";
     unsigned int i = crc32(testString, c_salt) % c_numBuckets;
 
-    // also useable as an ID
+    // This macro list is used for:
+    //  * making the enum
+    //  * making the cases in the switch statement
+    // D.R.Y. - Don't Repeat Yourself.
+    // Fewer moving parts = fewer errors, but admittedly is harder to understand vs redundant code.
+    #define ENUM_VALUE_LIST \
+        VALUE(Identifier_A) \
+        VALUE(Identifier_B) \
+        VALUE(Identifier_C) \
+        VALUE(Identifier_D) \
+        VALUE(Identifier_E) \
+        VALUE(Identifier_F) \
+        VALUE(Identifier_G) \
+        VALUE(Identifier_H) 
+
+    // Make the enum values.
+    // Note these enum values are also usable as a contiguous ID if you needed one for an array index or similar.
+    // You could define an array with size EIdentifier::count for instance and use these IDs to index into it.
     enum class EIdentifier : unsigned char {
-        Identifier_A,
-        Identifier_B,
-        Identifier_C,
-        Identifier_D,
-        Identifier_E,
-        Identifier_F,
-        Identifier_G,
-        Identifier_H,
-        invalid = -1
+        #define VALUE(x) x,
+        ENUM_VALUE_LIST
+        #undef VALUE
+        count,
+        invalid = (unsigned char)-1
     };
 
-    // do hash assisted string comparison
-    #define StringCase(x) case (crc32(#x, c_salt) % c_numBuckets) : if(!strcmp(testString, #x)) identifier = EIdentifier::x; else identifier = EIdentifier::invalid; break;
+    // do a compile time hash assisted string comparison to convert string to enum
     EIdentifier identifier = EIdentifier::invalid;
     switch (i) {
-        StringCase(Identifier_A);
-        StringCase(Identifier_B);
-        StringCase(Identifier_C);
-        StringCase(Identifier_D);
-        StringCase(Identifier_E);
-        StringCase(Identifier_F);
-        StringCase(Identifier_G);
-        StringCase(Identifier_H);
+        #define VALUE(x) case (crc32(#x, c_salt) % c_numBuckets) : if(!strcmp(testString, #x)) identifier = EIdentifier::x; else identifier = EIdentifier::invalid; break;
+        ENUM_VALUE_LIST
+        #undef VALUE
         default: identifier = EIdentifier::invalid;
     }
-    #undef StringCase
+    
+    // undefine the enum value list
+    #undef ENUM_VALUE_LIST
 
     printf("string translated to enum value %i", identifier);
 }
@@ -300,7 +304,7 @@ int main(int argc, char **argv)
 
     //Snippet_CompileTimeMinimalPerfectHash();
 
-    Snippet_CompileTimeAssistedStringToEnum();
+    Snippet_CompileTimeHashAssistedStringToEnum();
 
     return 0;
 }
@@ -318,12 +322,6 @@ Blog:
  * x86 / x64 seem to have identical behavior
 - Also get some insights into how optimizer works
 - There are probably some interesting tests I didn't do.  If you think of any and do them, let us know the results! Also, results on different compilers would be interesting to analyze.
-
-* link to github project?
-* put this source.cpp file contents inline in the blog post. Or just the snippets that you want to illustrate.
-* Link to minimally perfect hash article you wrote up.
-
-
 
 * Simple Compile Time Hashing Snippet
  * put code snippet
@@ -376,6 +374,10 @@ Blog:
  ? is it really faster than other string compare methods?
  ? what about when there are hash collisions for strings?
   ? leave it for someone else to work out?
+
+* link to github project?
+* put this source.cpp file contents inline in the blog post. Or just the snippets that you want to illustrate.
+* Link to minimally perfect hash article you wrote up.
 
 
 */
