6aafb4dd6f0f4aee9fecbbbd572cd3ba590ad062
Atrix256 <alan.wolfe@gmail.com>
2016-09-26 13:30:10 -0700

    ArrayVsSwitch done

--- ArrayAccessSpeeds/Source.cpp
+++ ArrayAccessSpeeds/Source.cpp
@@ -5,8 +5,20 @@
 #include <stdint.h>
 #include <limits>
 #include <chrono>
+#include <algorithm>
 
-#include "Tests.h"
+// run time parameters
+#define GENERATE_CODE() 0
+#define DO_TEST()       1
+static const size_t c_testRepeatCount = 10000;
+
+// code generation parameters
+typedef uint32_t TTestType;
+#define TTestTypeName "uint32_t"
+#define TTestTypePrintf "%u"
+static const size_t c_generateNumValues = 5000;
+static const size_t c_sparseNumValues = 1000;
+static_assert(c_sparseNumValues <= c_generateNumValues,"c_sparseNumValues must be less than or equation to c_generateNumValues.");
 
 struct SBlockTimer
 {
@@ -26,10 +38,6 @@ struct SBlockTimer
     const char* m_label;
 };
 
-typedef uint32_t TTestType;
-#define TTestTypeName "uint32_t"
-#define TTestTypePrintf "%u"
-
 TTestType Random()
 {
     static std::random_device rd;
@@ -40,7 +48,7 @@ TTestType Random()
     return dist(mt);
 }
 
-void GenerateCode (size_t numValues)
+void GenerateCode ()
 {
 
     FILE *file = fopen("Tests.h", "w+t");
@@ -49,129 +57,346 @@ void GenerateCode (size_t numValues)
 
     // generate the numbers
     std::vector<TTestType> randomValues;
-    randomValues.resize(numValues);
+    randomValues.resize(c_generateNumValues);
     for (TTestType& v : randomValues)
         v = Random();
 
-    // print out how many values there are, and the value type
-    fprintf(file, "static const size_t c_numValues = %u;\n\n", numValues);
+    // print out how many values there are
+    fprintf(file, "static const size_t c_numValues = %u;\n\n", c_generateNumValues);
+
+    // make a shuffle order array
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<size_t> shuffleOrder;
+    shuffleOrder.resize(c_generateNumValues);
+    for (size_t i = 0; i < c_generateNumValues; ++i)
+        shuffleOrder[i] = i;
+    std::shuffle(shuffleOrder.begin(), shuffleOrder.end(), g);
+    fprintf(file, "static const size_t c_shuffleOrder[%u] = {", c_generateNumValues);
+    fprintf(file, TTestTypePrintf, shuffleOrder[0]);
+    for (size_t index = 1; index < c_generateNumValues; ++index)
+        fprintf(file, ","TTestTypePrintf, shuffleOrder[index]);
+    fprintf(file, "};\n\n");
+
+    // make a sparse shuffle order array
+    fprintf(file, "static const size_t c_sparseShuffleOrder[%u] = {", c_sparseNumValues);
+    fprintf(file, TTestTypePrintf, shuffleOrder[0]);
+    for (size_t index = 1; index < c_sparseNumValues; ++index)
+        fprintf(file, ","TTestTypePrintf, shuffleOrder[index]);
+    fprintf(file, "};\n\n");
 
     // make a c style array
-    fprintf(file, "static const " TTestTypeName " carray[%u] = {", numValues);
+    fprintf(file, "static const " TTestTypeName " carray[%u] = {", c_generateNumValues);
     fprintf(file, TTestTypePrintf, randomValues[0]);
-    for (size_t index = 1; index < numValues; ++index)
+    for (size_t index = 1; index < c_generateNumValues; ++index)
         fprintf(file, ","TTestTypePrintf,randomValues[index]);
     fprintf(file,"};\n\n");
 
     // make a std::array style array
-    fprintf(file, "static const std::array<" TTestTypeName ", %u> stdarray = {", numValues);
+    fprintf(file, "static const std::array<" TTestTypeName ", %u> stdarray = {", c_generateNumValues);
     fprintf(file, TTestTypePrintf, randomValues[0]);
-    for (size_t index = 1; index < numValues; ++index)
+    for (size_t index = 1; index < c_generateNumValues; ++index)
         fprintf(file, ","TTestTypePrintf, randomValues[index]);
     fprintf(file, "}; \n\n");
 
     // make a std::vector style array
     fprintf(file, "static const std::vector<" TTestTypeName "> stdvector = {");
     fprintf(file, TTestTypePrintf, randomValues[0]);
-    for (size_t index = 1; index < numValues; ++index)
+    for (size_t index = 1; index < c_generateNumValues; ++index)
         fprintf(file, ","TTestTypePrintf, randomValues[index]);
     fprintf(file, "}; \n\n");
 
-    // make a pointer to dynamic memory (the std::vector's memory!)
-    fprintf(file, "static const "TTestTypeName"* dynamicmemory = &stdvector[0]; \n\n");
-
-    // make the switch statement
+    // make the switch statement function
     fprintf(file, "inline " TTestTypeName " LookupSwitch (size_t index) {\n");
     fprintf(file, "    switch(index) {\n");
-    for (size_t index = 0; index < numValues; ++index)
+    for (size_t index = 0; index < c_generateNumValues; ++index)
         fprintf(file, "        case %u: return " TTestTypePrintf ";\n", index, randomValues[index]);
-    fprintf(file, "    }\n    return -1;\n}\n");
+    fprintf(file, "    }\n    ((int*)0)[0] = 0; return -1;\n}\n\n");
+
+    // make the sparse switch statement function
+    std::vector<size_t> sparseRandomValues;
+    for (size_t i = 0; i < c_generateNumValues; ++i)
+    {
+        if (std::find(&shuffleOrder[0], &shuffleOrder[c_sparseNumValues], i) != &shuffleOrder[c_sparseNumValues])
+            sparseRandomValues.push_back(i);
+    }
+    fprintf(file, "inline " TTestTypeName " SparseLookupSwitch (size_t index) {\n");
+    fprintf(file, "    switch(index) {\n");
+    for (size_t index = 0; index < c_sparseNumValues; ++index)
+        fprintf(file, "        case %u: return " TTestTypePrintf ";\n", sparseRandomValues[index], randomValues[sparseRandomValues[index]]);
+    fprintf(file, "    }\n    ((int*)0)[0] = 0; return -1;\n}\n");
 
     // close the file
     fclose(file);
 }
 
+#if DO_TEST()
+    #include "Tests.h"
+#endif
+
 int main(int argc, char **argv)
 {
-    // To make the comparison code
-    //GenerateCode(5000);
+    #if GENERATE_CODE()
+        // To make the comparison code
+        GenerateCode();
+    #endif
 
-    static const size_t c_times = 1000;
+    #if DO_TEST()
+        // make our dynamic memory
+        TTestType* dynamicmemory = new TTestType[c_numValues];
+        memcpy(dynamicmemory, &stdvector[0], sizeof(TTestType)*c_numValues);
 
-    // do the tests!
-    TTestType sum1 = 0;
-    {
-        SBlockTimer timer("c array sum sequence");
-        for (size_t times = 0; times < c_times; ++times)
-            for (size_t i = 0; i < c_numValues; ++i)
-                sum1 += carray[i];
-    }
-    TTestType sum2 = 0;
-    {
-        SBlockTimer timer("std::array sum sequence");
-        for (size_t times = 0; times < c_times; ++times)
-            for (size_t i = 0; i < c_numValues; ++i)
-                sum2 += stdarray[i];
-    }
-    TTestType sum3 = 0;
-    {
-        SBlockTimer timer("std::vector sum sequence");
-        for (size_t times = 0; times < c_times; ++times)
-            for (size_t i = 0; i < c_numValues; ++i)
-                sum3 += stdvector[i];
-    }
-    TTestType sum4 = 0;
-    {
-        SBlockTimer timer("dynamic memory sum sequence");
-        for (size_t times = 0; times < c_times; ++times)
-            for (size_t i = 0; i < c_numValues; ++i)
-                sum4 += dynamicmemory[i];
-    }
-    TTestType sum5 = 0;
-    {
-        SBlockTimer timer("switch sum sequence");
-        for (size_t times = 0; times < c_times; ++times)
-            for (size_t i = 0; i < c_numValues; ++i)
-                sum5 += LookupSwitch(i);
-    }
+        // Sequential Sum Tests
+        TTestType sum = 0;
+        {
+            printf("Sequential Sum Timings:\n");
 
-    printf("sums %u\n", sum1 + sum2 + sum3 + sum4 + sum5);
+            {
+                SBlockTimer timer("  c array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += carray[i];
+            }
+            {
+                SBlockTimer timer("  std::array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += stdarray[i];
+            }
+            {
+                SBlockTimer timer("  std::vector");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += stdvector[i];
+            }
+            {
+                SBlockTimer timer("  dynamic memory");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += dynamicmemory[i];
+            }
+            {
+                SBlockTimer timer("  Switch");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += LookupSwitch(i);
+            }
+        }
+
+        // shuffle sum tests
+        {
+            printf("\nShuffle Sum Timings:\n");
+
+            {
+                SBlockTimer timer("  c array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += carray[c_shuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  std::array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += stdarray[c_shuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  std::vector");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += stdvector[c_shuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  dynamic memory");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += dynamicmemory[c_shuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  Switch");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_numValues; ++i)
+                        sum += LookupSwitch(c_shuffleOrder[i]);
+            }
+        }
+
+        // sparse shuffle sum tests
+        {
+            printf("\nSparse Shuffle Sum Timings:\n");
+
+            {
+                SBlockTimer timer("  c array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_sparseNumValues; ++i)
+                        sum += carray[c_sparseShuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  std::array");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_sparseNumValues; ++i)
+                        sum += stdarray[c_sparseShuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  std::vector");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_sparseNumValues; ++i)
+                        sum += stdvector[c_sparseShuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  dynamic memory");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_sparseNumValues; ++i)
+                        sum += dynamicmemory[c_sparseShuffleOrder[i]];
+            }
+            {
+                SBlockTimer timer("  Switch");
+                for (size_t times = 0; times < c_testRepeatCount; ++times)
+                    for (size_t i = 0; i < c_sparseNumValues; ++i)
+                        sum += LookupSwitch(c_sparseShuffleOrder[i]);
+            }
+        }
+
+        printf("\n");
+        system("pause");
+        printf("Sum = %i\n", sum); // to keep sum, and all the things that calculate it from getting optimized away!
+    #endif
+
     return 0;
 }
 
 /*
 
-TODO:
-* measure array access speeds: sequential and random shuffle.
-* debug and release
-* std::array, std::vector, const c array, dynamic memory (point to vector's memory maybe?), switch statement
-* time blocks
+BLOG: Is Code Faster than Data? switch vs array
+Question: how does array access speed compare to switch statement speed?
+ * x86/x64
+ * sequential, random, sparse
 
-! the switch statement is the slowest so far.
- ? maybe its cause it has tests and jumps instead of just memory lookups?
- ? what does this mean for compile time hashing? maybe we need to make a data structure, not a switch statement? it does a strcmp against the string and sets to the enum value if equals, else invalid. nullptr/invalid for invalid entries?
+Testing Details
+ * vs 2013 community edition
+ * 5000 random uint32's
+ * c style array
+ * std::array
+ * std::vector
+ * dynamic allocated memory
+ * Switch statement function to look up a value by index
+ * Go through each array 10000 times
+ * Sequential Sum:
+  * add up all the values in each array for the arrays
+  * call the switch function for each index and sum it up
+ * Shuffle Sum:
+  * Same as sequential sum, but the indices are accessed in a shuffled order
+ * Sparse Sum:
+  * same as Shuffle sum, but only doing first 1000 items of the shuffle
+  * Uses a speciailized switch function which has only those 1000 items in it.  Simulating unused values getting stripped out.
 
-! if i turn the numbers up to 100k, the compiler errors out.
- ? what is limit?
- ? what is limit of switch?
+! link to code, in github!
 
-* try making switch inline?
+Results:
+* switch statement is A LOT slower than memory!
+ * makes sense when you look at asm.
+  1) Overhead of function call
+  2) we compare the switch value against max to see if it's in range. (can avoid this with __assume(0) apparently)
+  3) we then calculate our jump, possibly with an extra layer of indirection.
+  4) we jump to that location and return a constant.
+ * VS array access
+  1) calculate memory location based on index.
+  2) return value at index
+ What if we inline the switch statement function
+ * Don't know.. it made the compiler go into "Generating Code" for a long time, never saw it finish!
 
-* analyze asm in debug and release? x86/x64?
+Follow up questions:
+ * How does switch statement speed compare to hash tables?
+ * If hash tables are faster than switch statements, the "hash assisted string to enum" code I made is probably slower than a hash table
+  * The compile time assurance of no hash collisions is really nice though.
+  ? can we get the benefits of both?
 
-Next:
-* analyze sparse array as switch vs something else
 
-Then:
-* hashinator and sparse array utility next!
+Win32 Debug
+    Sequential Sum Timings:
+      c array: 108.48 ms
+      std::array: 1135.56 ms
+      std::vector: 2160.68 ms
+      dynamic memory: 109.56 ms
+      Switch: 1687.78 ms
 
-?use of sparse array?
- * when you are given input but don't care about all of it
- * could also make multiple things point at the same memory if same value!!
+    Shuffle Sum Timings:
+      c array: 109.09 ms
+      std::array: 1093.76 ms
+      std::vector: 2156.12 ms
+      dynamic memory: 102.07 ms
+      Switch: 1749.74 ms
 
-* utility programs to generate code from source data that also uses constexpr
-* Make it so you can also load data from.a file dynamically, for large data sets.
-* See where you can use fpe (encryption) instead of hashing to easily ensure no collisions.
- * Maybe best there for sparse array? not sure how hash table would do
+    Sparse Shuffle Sum Timings:
+      c array: 20.01 ms
+      std::array: 218.15 ms
+      std::vector: 433.83 ms
+      dynamic memory: 21.03 ms
+      Switch: 294.71 ms
+
+Win32 Release:
+    Sequential Sum Timings:
+      c array: 4.49 ms
+      std::array: 3.50 ms
+      std::vector: 4.50 ms
+      dynamic memory: 4.50 ms
+      Switch: 453.49 ms
+
+    Shuffle Sum Timings:
+      c array: 21.03 ms
+      std::array: 21.52 ms
+      std::vector: 22.00 ms
+      dynamic memory: 22.02 ms
+      Switch: 487.85 ms
+
+    Sparse Shuffle Sum Timings:
+      c array: 4.00 ms
+      std::array: 4.00 ms
+      std::vector: 4.00 ms
+      dynamic memory: 4.50 ms
+      Switch: 64.05 ms
+
+
+x64 Debug:
+    Sequential Sum Timings:
+      c array: 110.08 ms
+      std::array: 501.36 ms
+      std::vector: 576.91 ms
+      dynamic memory: 108.09 ms
+      Switch: 1186.33 ms
+
+    Shuffle Sum Timings:
+      c array: 107.08 ms
+      std::array: 491.34 ms
+      std::vector: 569.40 ms
+      dynamic memory: 101.57 ms
+      Switch: 1264.53 ms
+
+    Sparse Shuffle Sum Timings:
+      c array: 21.52 ms
+      std::array: 99.07 ms
+      std::vector: 116.08 ms
+      dynamic memory: 20.51 ms
+      Switch: 193.64 ms
+
+x64 Release:
+    Sequential Sum Timings:
+      c array: 3.99 ms
+      std::array: 3.51 ms
+      std::vector: 4.50 ms
+      dynamic memory: 3.51 ms
+      Switch: 490.96 ms
+
+    Shuffle Sum Timings:
+      c array: 23.52 ms
+      std::array: 18.53 ms
+      std::vector: 17.51 ms
+      dynamic memory: 18.02 ms
+      Switch: 524.11 ms
+
+    Sparse Shuffle Sum Timings:
+      c array: 3.00 ms
+      std::array: 3.02 ms
+      std::vector: 3.50 ms
+      dynamic memory: 3.00 ms
+      Switch: 65.56 ms
 
 */
