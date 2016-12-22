ab751888e719f2295154520b8c60adb1691d0cdb
Atrix256 <alan.wolfe@gmail.com>
2016-12-22 12:05:20 -0800

    Simplified code / more tests
    Less matrix math when not needed, more to the point.

--- OnlineLeastSquaresFitting/Source.cpp
+++ OnlineLeastSquaresFitting/Source.cpp
@@ -11,10 +11,7 @@ using TMatrix = std::array<TVector<N>, M>;
 template<size_t N>
 using TSquareMatrix = TMatrix<N,N>;
 
-// data points are 2d vectors
 typedef TVector<2> TDataPoint; 
-template <size_t NUMDATAPOINTS>
-using TDataPointList = std::array<TDataPoint, NUMDATAPOINTS>;
 
 //====================================================================
 template <size_t N>
@@ -139,163 +136,95 @@ bool InvertMatrix<2> (const TSquareMatrix<2>& in, TSquareMatrix<2>& out)
 }
 
 //====================================================================
-constexpr size_t ATACompressedStorageSize (size_t degree)
-{
-    return degree == 0 ? 1 : ATACompressedStorageSize(degree - 1) * 2 + 1;
-}
-
-//====================================================================
 template <size_t DEGREE>  // 1 = linear, 2 = quadratic, etc
 class COnlineLeastSquaresFitter
 {
 public:
-
-    template <size_t NUMDATAPOINTS>
-    COnlineLeastSquaresFitter (const TDataPointList<NUMDATAPOINTS>& initialData)
+    COnlineLeastSquaresFitter ()
     {
-        static_assert(NUMDATAPOINTS >= DEGREE + 1, "There needs to be DEGREE+1 initial data points supplied to the constructor of COnlineLeastSquaresFitter.");
-
-        // calculate matrix "A"
-        // A_jk = x_j ^ k
-        TMatrix<NUMDATAPOINTS, DEGREE + 1> A;
-        for (size_t j = 0; j < NUMDATAPOINTS; ++j)
-            for (size_t k = 0; k < DEGREE + 1; ++k)
-                A[j][k] = std::pow(initialData[j][0], float(k));
-
-        // calculate matrix "ATA"  - A transposed times A
-        TMatrix<DEGREE + 1, NUMDATAPOINTS> AT;
-        TransposeMatrix(A, AT);
-        TMatrix<DEGREE + 1, DEGREE + 1> ATA;
-        for (size_t j = 0; j < DEGREE + 1; ++j)
-            for (size_t k = 0; k < DEGREE + 1; ++k)
-                ATA[j][k] = DotProduct(AT[j], AT[k]);
-
-        // Calculate vector "ATY" - A transposed times the Y values of the input data
-        TMatrix<2, NUMDATAPOINTS> initialDataTransposed;
-        TransposeMatrix(initialData, initialDataTransposed);
-        for (size_t i = 0; i < DEGREE + 1; ++i)
-            m_ATY[i] = DotProduct(AT[i], initialDataTransposed[1]);
-
-        // Compress the ATA matrix
-        CompressATAMatrix(ATA);
+        // initialize our sums to zero
+        std::fill(m_SummedPowersX.begin(), m_SummedPowersX.end(), 0.0f);
+        std::fill(m_SummedPowersXTimesY.begin(), m_SummedPowersXTimesY.end(), 0.0f);
     }
 
     void AddDataPoint (const TDataPoint& dataPoint)
     {
-        // Given a new data point (x_j, y_j), we need to adjust...
-        // 1) ATA_ik += x_j^(i+k)
-        // 2) ATY_i += x_j^i * y_j
-        // where i, k is in [0, DEGREE]
+        // add the summed powers of the x value
+        for (size_t i = 0; i < m_SummedPowersX.size(); ++i)
+            m_SummedPowersX[i] += std::pow(dataPoint[0], float(i));
 
-        // Decompress the ATA matrix so we can update it.
-        // It would be more efficient to update the compressed ATA matrix, but doing this for clarity of the code.
-        TMatrix<DEGREE + 1, DEGREE + 1> ATA;
-        DecompressATAMatrix(ATA);
-
-        // 1) ATA_ik += x_j^(i+k)
-        for (size_t i = 0; i < DEGREE + 1; ++i)
-            for (size_t k = 0; k < DEGREE + 1; ++k)
-                ATA[i][k] += std::pow(dataPoint[0], float(i+k));
-
-        // Recompress the ATA matrix since we are done modifying it
-        CompressATAMatrix(ATA);
-
-        // 2) ATY_i += x_j^i * y_j
-        for (size_t i = 0; i < DEGREE + 1; ++i)
-            m_ATY[i] += std::pow(dataPoint[0], float(i)) * dataPoint[1];
+        // add the summed powers of the x value, multiplied by the y value
+        for (size_t i = 0; i < m_SummedPowersXTimesY.size(); ++i)
+            m_SummedPowersXTimesY[i] += std::pow(dataPoint[0], float(i)) * dataPoint[1];
     }
 
-    bool CalculateCoefficients (TVector<DEGREE+1>& coefficients)
+    bool CalculateCoefficients (TVector<DEGREE+1>& coefficients) const
     {
-        // Decompress the ATA matrix
-        TMatrix<DEGREE + 1, DEGREE + 1> ATA;
-        DecompressATAMatrix(ATA);
+        // initialize all coefficients to zero
+        std::fill(coefficients.begin(), coefficients.end(), 0.0f);
 
-        // calculate inverse matrix of ATA matrix
-        TMatrix<DEGREE + 1, DEGREE + 1> ATAInverse;
+        // calculate the coefficients
+        return CalculateCoefficientsInternal<DEGREE>(coefficients);
+    }
+
+private:
+
+    template <size_t EFFECTIVEDEGREE>
+    bool CalculateCoefficientsInternal (TVector<DEGREE + 1>& coefficients) const
+    {
+        // if we don't have enough data points for this degree, try one degree less
+        if (m_SummedPowersX[0] <= EFFECTIVEDEGREE)
+            return CalculateCoefficientsInternal<EFFECTIVEDEGREE - 1>(coefficients);
+
+        // Make the ATA matrix
+        TMatrix<EFFECTIVEDEGREE + 1, EFFECTIVEDEGREE + 1> ATA;
+        for (size_t i = 0; i < EFFECTIVEDEGREE + 1; ++i)
+            for (size_t j = 0; j < EFFECTIVEDEGREE + 1; ++j)
+                ATA[i][j] = m_SummedPowersX[i + j];
+
+        // calculate inverse of ATA matrix
+        TMatrix<EFFECTIVEDEGREE + 1, EFFECTIVEDEGREE + 1> ATAInverse;
         if (!InvertMatrix(ATA, ATAInverse))
             return false;
 
-        // calculate the coefficients
-        for (size_t i = 0; i < DEGREE + 1; ++i)
-            coefficients[i] = DotProduct(ATAInverse[i], m_ATY);
-
+        // calculate the coefficients for this degree. The higher ones are already zeroed out.
+        TVector<EFFECTIVEDEGREE + 1> summedPowersXTimesY;
+        std::copy(m_SummedPowersXTimesY.begin(), m_SummedPowersXTimesY.begin() + EFFECTIVEDEGREE + 1, summedPowersXTimesY.begin());
+        for (size_t i = 0; i < EFFECTIVEDEGREE + 1; ++i)
+            coefficients[i] = DotProduct(ATAInverse[i], summedPowersXTimesY);
         return true;
     }
 
-private:
-    void DecompressATAMatrix (TMatrix<DEGREE + 1, DEGREE + 1>& ATA)
+    // Base case when no points are given, or if you are fitting a degree 0 curve to the data set.
+    template <>
+    bool CalculateCoefficientsInternal<0> (TVector<DEGREE + 1>& coefficients) const
     {
-        // ATA is symmetrical across the diagonal so we only need to store the diagonal and top half.
-        // We can just restore those values and reflect the top half for the lower half.
-        size_t inputIndex = 0;
-        for (size_t i = DEGREE + 1; i > 0; --i)
-        {
-            size_t row = (DEGREE + 1) - i;
-            for (size_t j = 0; j < i; ++j)
-            {
-                ATA[row][row + j] = m_ATACompressed[inputIndex];
-                ++inputIndex;
-            }
-        }
-        // reflect
-        for (size_t i = 1; i < DEGREE + 1; ++i)
-        {
-            for (size_t j = 0; j < i; ++j)
-                ATA[i][j] = ATA[j][i];
-        }
+        if (m_SummedPowersX[0] > 0.0f)
+            coefficients[0] = m_SummedPowersXTimesY[0] / m_SummedPowersX[0];
+        return true;
     }
 
-    void CompressATAMatrix (const TMatrix<DEGREE + 1, DEGREE + 1>& ATA)
-    {
-        // ATA is symmetrical across the diagonal so we only need to store the diagonal and top half.
-        size_t outputIndex = 0;
-        for (size_t i = DEGREE + 1; i > 0; --i)
-        {
-            size_t row = (DEGREE + 1) - i;
-            for (size_t j = 0; j < i; ++j)
-            {
-                m_ATACompressed[outputIndex] = ATA[row][row + j];
-                ++outputIndex;
-            }
-        }
-    }
-
-private:
-    // Total storage space needed is:
-    // y = 0.5*x^2 + 2.5x + 2
+    // Total storage space (# of floats) needed is 3 * DEGREE + 2
     // Where y is number of values that need to be stored and x is the degree of the polynomial
-    TVector<DEGREE+1>                           m_ATY;
-    TVector<ATACompressedStorageSize(DEGREE)>   m_ATACompressed;
+    TVector<(DEGREE + 1) * 2 - 1>   m_SummedPowersX;
+    TVector<DEGREE + 1>             m_SummedPowersXTimesY;
 };
 
 //====================================================================
-template <size_t DEGREE, size_t A, size_t B>
-void DoTest (const TDataPointList<A>& initialData, const TDataPointList<B>& otherData)
+template <size_t DEGREE>
+void DoTest(const std::initializer_list<TDataPoint>& data)
 {
-	printf("========================================\n");
-	printf("Fitting a curve of degree %zi\n", DEGREE);
-	printf("========================================\n");
+	printf("Fitting a curve of degree %zi to %zi data points:\n", DEGREE, data.size());
 
-	// show initial data
-	printf("%zi initial data points:\n", A);
-	for (size_t i = 0; i < A; ++i)
-		printf("  %zi: (%0.2f, %0.2f)\n", i + 1, initialData[i][0], initialData[i][1]);
+    COnlineLeastSquaresFitter<DEGREE> fitter;
 
-	// show other data
-	if (B > 0)
-	{
-		printf("%zi other data points:\n", B);
-		for (size_t i = 0; i < B; ++i)
-			printf("  %zi: (%0.2f, %0.2f)\n", i + 1, otherData[i][0], otherData[i][1]);
-	}
+	// show data
+    for (const TDataPoint& dataPoint : data)
+		printf("  (%0.2f, %0.2f)\n", dataPoint[0], dataPoint[1]);
 
-	// fit initial data
-	COnlineLeastSquaresFitter<DEGREE> fitter(initialData);
-
-	// fit other data
-	for (size_t i = 0; i < B; ++i)
-		fitter.AddDataPoint(otherData[i]);
+	// fit data
+    for (const TDataPoint& dataPoint : data)
+        fitter.AddDataPoint(dataPoint);
 
 	// calculate coefficients if we can
 	TVector<DEGREE+1> coefficients;
@@ -309,11 +238,14 @@ void DoTest (const TDataPointList<A>& initialData, const TDataPointList<B>& othe
 	// print the polynomial
 	bool firstTerm = true;
 	printf("y = ");
+    bool showedATerm = false;
 	for (int i = (int)coefficients.size() - 1; i >= 0; --i)
 	{
 		// don't show zero terms
 		if (std::abs(coefficients[i]) < 0.00001f)
 			continue;
+
+        showedATerm = true;
 
 		// show an add or subtract between terms
 		float coefficient = coefficients[i];
@@ -335,84 +267,103 @@ void DoTest (const TDataPointList<A>& initialData, const TDataPointList<B>& othe
 		if (i > 1)
 			printf("^%i", i);
 	}
-	printf("\n");
-	printf("\n");
+    if (!showedATerm)
+        printf("0");
+	printf("\n\n");
 }
 
 //====================================================================
 int main (int argc, char **argv)
 {
-	// Linear - 3 initial data points
-	{
-        TDataPointList<3> initialData { {
-            { { 1.0f, 2.0f } },
-            { { 2.0f, 4.0f } },
-			{ { 3.0f, 6.0f } }
-        } };
+	// Point - 1 data points
+    DoTest<0>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+        }
+    );
 
-		TDataPointList<0> otherData { {
-		} };
+	// Point - 2 data points
+    DoTest<0>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+            TDataPoint{ 2.0f, 4.0f },
+        }
+    );
 
-		DoTest<1, initialData.size(), otherData.size()>(initialData, otherData);
-	}
+	// Linear - 2 data points
+    DoTest<1>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+            TDataPoint{ 2.0f, 4.0f },
+        }
+    );
 
-	// Linear - 2 initial data points, 1 other data point
-	{
-        TDataPointList<2> initialData { {
-            { { 1.0f, 2.0f } },
-            { { 2.0f, 4.0f } }
-        } };
+	// Linear - 3 colinear data points
+    DoTest<1>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+            TDataPoint{ 2.0f, 4.0f },
+            TDataPoint{ 3.0f, 6.0f },
+        }
+    );
 
-		TDataPointList<1> otherData { {
-			{ { 3.0f, 6.0f } },
-		} };
+	// Linear - 3 non colinear data points
+    DoTest<1>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+            TDataPoint{ 2.0f, 4.0f },
+            TDataPoint{ 3.0f, 5.0f },
+        }
+    );
 
-		DoTest<1, initialData.size(), otherData.size()>(initialData, otherData);
-	}
+	// Quadratic - 3 colinear data points
+    DoTest<2>(
+        {
+            TDataPoint{ 1.0f, 2.0f },
+            TDataPoint{ 2.0f, 4.0f },
+            TDataPoint{ 3.0f, 6.0f },
+        }
+    );
 
-	// Quadratic - 3 initial data points (colinear)
-	{
-        TDataPointList<3> initialData { {
-            { { 1.0f, 2.0f } },
-            { { 2.0f, 4.0f } },
-			{ { 3.0f, 6.0f } }
-        } };
+	// Quadratic - 3 data points
+    DoTest<2>(
+        {
+            TDataPoint{ 1.0f, 5.0f },
+            TDataPoint{ 2.0f, 16.0f },
+            TDataPoint{ 3.0f, 31.0f },
+        }
+    );
 
-		TDataPointList<0> otherData { {
-		} };
+	// Cubic - 4 data points
+    DoTest<3>(
+        {
+            TDataPoint{ 1.0f, 5.0f },
+            TDataPoint{ 2.0f, 16.0f },
+            TDataPoint{ 3.0f, 31.0f },
+            TDataPoint{ 4.0f, 16.0f },
+        }
+    );
 
-		DoTest<2, initialData.size(), otherData.size()>(initialData, otherData);
-	}
+	// Cubic - 2 data points
+    DoTest<3>(
+        {
+            TDataPoint{ 1.0f, 7.0f },
+            TDataPoint{ 3.0f, 17.0f },
+        }
+    );
 
-	// Quadratic - 3 initial data points, 0 other data points
-	{
-		// Fit some initial data points
-		TDataPointList<3> initialData { {
-			{ { 1.0f, 5.0f } },
-			{ { 2.0f, 16.0f } },
-			{ { 3.0f, 31.0f } }
-		} };
+	// Cubic - 1 data point
+    DoTest<3>(
+        {
+            TDataPoint{ 1.0f, 7.0f },
+        }
+    );
 
-		TDataPointList<0> otherData { {
-		} };
-
-		DoTest<2, initialData.size(), otherData.size()>(initialData, otherData);
-	}
-
-	// Cubic  - 4 initial data points, 0 other data points
-	{
-		TDataPointList<4> initialData { {
-			{ { 1.0f, 5.0f } },
-			{ { 2.0f, 16.0f } },
-			{ { 3.0f, 31.0f } },
-			{ { 4.0f, 16.0f } }
-		} };
-
-		TDataPointList<0> otherData { {
-		} };
-
-		DoTest<3, initialData.size(), otherData.size()>(initialData, otherData);
-	}
+	// Cubic - 0 data points
+    DoTest<3>(
+        {
+        }
+    );
 
     system("pause");
     return 0;
