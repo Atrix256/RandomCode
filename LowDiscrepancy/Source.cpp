65668ab4f68e8255404499d6cb88698263187de3
Alan Wolfe <alan.wolfe@gmail.com>
2017-05-11 13:06:45 -0700

    1d progress

--- LowDiscrepancy/Source.cpp
+++ LowDiscrepancy/Source.cpp
@@ -7,17 +7,20 @@
 
 typedef uint8_t uint8;
 
-#define IMAGE_WIDTH 400
+#define NUM_SAMPLES 100
+
+#define IMAGE_WIDTH 600
 #define IMAGE1D_HEIGHT 50
 #define IMAGE_PAD   30
 
 #define IMAGE_CENTERX ((IMAGE_WIDTH+IMAGE_PAD*2)/2)
 #define IMAGE1D_CENTERY ((IMAGE1D_HEIGHT+IMAGE_PAD*2)/2)
 
-#define AXIS_HEIGHT 20
-#define DATA_SIZE 10
+#define AXIS_HEIGHT 40
+#define DATA_HEIGHT 20
+#define DATA_WIDTH 2
 
-#define COLOR_FILL SColor(255, 255, 255)
+#define COLOR_FILL SColor(255,255,255)
 #define COLOR_AXIS SColor(0, 0, 0)
 #define COLOR_DATAPOINT SColor(255, 0, 0)
 
@@ -65,12 +68,12 @@ bool SaveImage (const char *fileName, const SImageData &image)
     header.bfOffBits = 54;
   
     infoHeader.biSize = 40;
-    infoHeader.biWidth = image.m_width;
-    infoHeader.biHeight = image.m_height;
+    infoHeader.biWidth = (LONG)image.m_width;
+    infoHeader.biHeight = (LONG)image.m_height;
     infoHeader.biPlanes = 1;
     infoHeader.biBitCount = 24;
     infoHeader.biCompression = 0;
-    infoHeader.biSizeImage = image.m_pixels.size();
+    infoHeader.biSizeImage = (DWORD) image.m_pixels.size();
     infoHeader.biXPelsPerMeter = 0;
     infoHeader.biYPelsPerMeter = 0;
     infoHeader.biClrUsed = 0;
@@ -123,43 +126,83 @@ void ImageBox (SImageData& image, size_t x1, size_t x2, size_t y1, size_t y2, co
 }
 
 //======================================================================================
-void ImageHLine (SImageData& image, size_t x1, size_t x2, size_t y, const SColor& color)
+void DrawDecorations1DPre (SImageData& image)
 {
-	ImageBox(image, x1, x2, y, y + 1, color);
+    ImageClear(image, COLOR_FILL);
 }
 
 //======================================================================================
-void ImageVLine (SImageData& image, size_t x, size_t y1, size_t y2, const SColor& color)
+void DrawDecorations1DPost (SImageData& image)
 {
-	ImageBox(image, x, x+1, y1, y2, color);
+    // draw the axes lines
+
+    // Horizontal line
+    ImageBox(image, IMAGE_PAD, IMAGE_WIDTH + IMAGE_PAD, IMAGE1D_CENTERY, IMAGE1D_CENTERY + 1, COLOR_AXIS);
+
+    // vertical lines
+    ImageBox(image, IMAGE_PAD, IMAGE_PAD + 1, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
+    ImageBox(image, IMAGE_WIDTH + IMAGE_PAD, IMAGE_WIDTH + IMAGE_PAD + 1, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
 }
 
 //======================================================================================
+SColor DataPointColor (size_t sampleIndex, size_t sampleCount)
+{
+    SColor ret;
+    float percent = (float(sampleIndex) / (float(sampleCount) - 1.0f));
+
+    ret.R = uint8((1.0f - percent) * 255.0f);
+    ret.G = 0;
+    ret.B = uint8(percent * 255.0f);
+
+    float mag = (float)sqrt(ret.R*ret.R + ret.G*ret.G + ret.B*ret.B);
+    ret.R = uint8((float(ret.R) / mag)*255.0f);
+    ret.G = uint8((float(ret.G) / mag)*255.0f);
+    ret.B = uint8((float(ret.B) / mag)*255.0f);
+
+    return ret;
+}
+
+//======================================================================================
+float RandomFloat (float min, float max)
+{
+    static std::random_device rd;
+    static std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> dist(min, max);
+    return dist(mt);
+}
+
+//======================================================================================
+template<bool JITTER>
 void TestUniform1D ()
 {
 	// create and clear the image
 	SImageData image;
 	ImageInit(image, IMAGE_WIDTH + IMAGE_PAD * 2, IMAGE1D_HEIGHT + IMAGE_PAD * 2);
-	ImageClear(image, COLOR_FILL);
+	
+    // setup the canvas
+    DrawDecorations1DPre(image);
 
 	// draw the sample points
-	static std::random_device rd;
-	static std::mt19937 mt(rd());
-	static std::uniform_real_distribution<float> dist(0.0f, 1.0f);
-	for (size_t i = 0; i < 50; ++i)
+    const float c_halfJitter = 1.0f / float((NUM_SAMPLES + 1) * 2);
+	for (size_t i = 0; i < NUM_SAMPLES; ++i)
 	{
-		float sample = float(i+1) / 51.0f;
+		float sample = float(i+1) / (float(NUM_SAMPLES)+1.0f);
+
+        if (JITTER)
+            sample += RandomFloat(-c_halfJitter, c_halfJitter);
+
 		size_t pos = size_t(sample * float(IMAGE_WIDTH)) + IMAGE_PAD;
-		ImageVLine(image, pos, IMAGE1D_CENTERY - DATA_SIZE / 2, IMAGE1D_CENTERY + DATA_SIZE / 2, COLOR_DATAPOINT);
+		ImageBox(image, pos, pos+1, IMAGE1D_CENTERY - DATA_HEIGHT / 2, IMAGE1D_CENTERY + DATA_HEIGHT / 2, DataPointColor(i, NUM_SAMPLES));
 	}
 
-	// draw the axes lines
-	ImageHLine(image, IMAGE_PAD, IMAGE_WIDTH + IMAGE_PAD, IMAGE1D_CENTERY, COLOR_AXIS);
-	ImageVLine(image, IMAGE_PAD, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
-	ImageVLine(image, IMAGE_WIDTH + IMAGE_PAD, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
+    // draw everything else
+    DrawDecorations1DPost(image);
 
 	// save the image
-	SaveImage("Uniform1D.bmp", image);
+    if (JITTER)
+	    SaveImage("1DUniformJitter.bmp", image);
+    else
+        SaveImage("1DUniform.bmp", image);
 }
 
 //======================================================================================
@@ -168,31 +211,182 @@ void TestUniformRandom1D ()
 	// create and clear the image
 	SImageData image;
 	ImageInit(image, IMAGE_WIDTH + IMAGE_PAD * 2, IMAGE1D_HEIGHT + IMAGE_PAD * 2);
-	ImageClear(image, COLOR_FILL);
+
+    // setup the canvas
+    DrawDecorations1DPre(image);
 
 	// draw the sample points
-	static std::random_device rd;
-	static std::mt19937 mt(rd());
-	static std::uniform_real_distribution<float> dist(0.0f, 1.0f);
-	for (size_t i = 0; i < 50; ++i)
+	for (size_t i = 0; i < NUM_SAMPLES; ++i)
 	{
-		float sample = dist(mt);
+        float sample = RandomFloat(0.0f, 1.0f);
 		size_t pos = size_t(sample * float(IMAGE_WIDTH)) + IMAGE_PAD;
-		ImageVLine(image, pos, IMAGE1D_CENTERY - DATA_SIZE / 2, IMAGE1D_CENTERY + DATA_SIZE / 2, COLOR_DATAPOINT);
+        ImageBox(image, pos, pos + 1, IMAGE1D_CENTERY - DATA_HEIGHT / 2, IMAGE1D_CENTERY + DATA_HEIGHT / 2, DataPointColor(i, NUM_SAMPLES));
 	}
 
-	// draw the axes lines
-	ImageHLine(image, IMAGE_PAD, IMAGE_WIDTH + IMAGE_PAD, IMAGE1D_CENTERY, COLOR_AXIS);
-	ImageVLine(image, IMAGE_PAD, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
-	ImageVLine(image, IMAGE_WIDTH + IMAGE_PAD, IMAGE1D_CENTERY - AXIS_HEIGHT / 2, IMAGE1D_CENTERY + AXIS_HEIGHT / 2, COLOR_AXIS);
+    // draw everything else
+    DrawDecorations1DPost(image);
 
 	// save the image
-	SaveImage("UniformRandom1D.bmp", image);
+	SaveImage("1DUniformRandom.bmp", image);
+}
+
+//======================================================================================
+template <size_t BASE, bool JITTER>
+void TestVanDerCorput ()
+{
+	// create and clear the image
+	SImageData image;
+	ImageInit(image, IMAGE_WIDTH + IMAGE_PAD * 2, IMAGE1D_HEIGHT + IMAGE_PAD * 2);
+	
+    // setup the canvas
+    DrawDecorations1DPre(image);
+
+	// draw the sample points
+	for (size_t i = 0; i < NUM_SAMPLES; ++i)
+	{
+        float sample = 0.0f;
+        float denominator = float(BASE);
+        size_t n = i;
+        while (n > 0)
+        {
+            size_t multiplier = n % BASE;
+            sample += float(multiplier) / denominator;
+            n = n / BASE;
+            denominator *= BASE;
+        }
+
+        if (JITTER)
+        {
+            float halfJitter = BASE;
+            size_t n = i / BASE;
+            while (n > 0)
+            {
+                halfJitter *= BASE;
+                n /= BASE;
+            }
+            halfJitter = 1.0f / (halfJitter * 2.0f);
+
+            sample += RandomFloat(-halfJitter, halfJitter);
+        }
+
+		size_t pos = size_t(sample * float(IMAGE_WIDTH)) + IMAGE_PAD;
+        ImageBox(image, pos, pos + 1, IMAGE1D_CENTERY - DATA_HEIGHT / 2, IMAGE1D_CENTERY + DATA_HEIGHT / 2, DataPointColor(i, NUM_SAMPLES));
+	}
+
+    // draw everything else
+    DrawDecorations1DPost(image);
+
+	// save the image
+    char fileName[256];
+    if (JITTER)
+        sprintf(fileName, "1DVanDerCorputJitter_%zu.bmp", BASE);
+    else
+        sprintf(fileName, "1DVanDerCorput_%zu.bmp", BASE);
+	SaveImage(fileName, image);
+}
+
+//======================================================================================
+void TestGoldenRatio ()
+{
+	// create and clear the image
+	SImageData image;
+	ImageInit(image, IMAGE_WIDTH + IMAGE_PAD * 2, IMAGE1D_HEIGHT + IMAGE_PAD * 2);
+	
+    // setup the canvas
+    DrawDecorations1DPre(image);
+
+	// draw the sample points
+    const float c_goldenRatioConugate = 0.61803398875f;
+    float sample = 0.0f;
+	for (size_t i = 0; i < NUM_SAMPLES; ++i)
+	{
+        sample = std::fmodf(sample + c_goldenRatioConugate, 1.0f);
+		size_t pos = size_t(sample * float(IMAGE_WIDTH)) + IMAGE_PAD;
+        ImageBox(image, pos, pos + 1, IMAGE1D_CENTERY - DATA_HEIGHT / 2, IMAGE1D_CENTERY + DATA_HEIGHT / 2, DataPointColor(i, NUM_SAMPLES));
+	}
+
+    // draw everything else
+    DrawDecorations1DPost(image);
+
+	// save the image
+	SaveImage("1DGoldenRatio.bmp", image);
 }
 
 //======================================================================================
 int main (int argc, char **argv)
 {
-	TestUniform1D();
-	TestUniformRandom1D();
+    // 1D tests
+    {
+        TestUniform1D<false>();
+        TestUniform1D<true>();
+
+        TestUniformRandom1D();
+
+        TestVanDerCorput<2, false>();
+        TestVanDerCorput<3, false>();
+        TestVanDerCorput<4, false>();
+
+        // TODO: try halving jitter amount maybe, or lower? or constant, small jitter amount?
+        TestVanDerCorput<2, true>();
+        TestVanDerCorput<3, true>();
+        TestVanDerCorput<4, true>();
+
+        // TODO: jitter!
+        TestGoldenRatio();
+    }
 }
+
+/*
+
+TODO:
+
+* color points based on sample number!
+
+* do fft for both 1d and 2d? does it make sense for sample points?
+ * if not, how do we analyze quality?
+ * maybe we calculate variance? i dunno...
+
+* 1d
+ * jitter uniform
+ * jitter van der corput
+ * jitter golden ratio
+ ? does hammersly come in 1d? I think so!
+ * ??
+
+? sobol sequence?
+
+* 2d
+ * halton sequence
+ * Hammersly
+ * uniform (grid)
+ * uniform random
+ ? jitter all 2d versions?
+ ? does fibanoci come in 2d?
+ * ??
+
+* van der corput based shuffling.
+ * can seed (kind of) by changing base
+ * 2d shuffle?
+
+? count the bits to make white noise into gaussian? or save this for another post?
+
+LINKS:
+fibanocci colors: http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+
+https://en.m.wikipedia.org/wiki/Halton_sequence
+
+https://en.m.wikipedia.org/wiki/Van_der_Corput_sequence
+
+grey codes
+
+format preserving encryption post
+
+blue noise posts:
+    http://momentsingraphics.de/?p=127
+    http://momentsingraphics.de/?p=148
+    http://www.joesfer.com/?p=108
+
+
+*
+ 
+*/
