ec48cb8b7363661f4392bde902b47c97c7fdf02b
Alan Wolfe <alan.wolfe@gmail.com>
2017-06-13 13:13:19 -0700

    Diffuse IBL Progress: Nearly have pass through

--- IBLDiffuse/Source.cpp
+++ IBLDiffuse/Source.cpp
@@ -9,14 +9,112 @@
 #include <atomic>
 
 #define FORCE_SINGLETHREADED() 0
-  
-typedef uint8_t uint8;
-typedef std::array<float, 3> TVector3;
-typedef std::array<float, 2> TVector2;
 
+// ==================================================================================================================
 const float c_pi = 3.14159265359f;
- 
-struct SImageData
+
+// ==================================================================================================================
+typedef uint8_t uint8;
+
+template <size_t N>
+using TVector = std::array<float, N>;
+
+typedef TVector<3> TVector3;
+typedef TVector<2> TVector2;
+
+// ==================================================================================================================
+template <size_t N>
+TVector<N> operator + (const TVector<N>& a, const TVector<N>& b)
+{
+    TVector<N> result;
+    for (size_t i = 0; i < N; ++i)
+        result[i] = a[i] + b[i];
+    return result;
+}
+
+template <size_t N>
+TVector<N> operator * (const TVector<N>& a, const TVector<N>& b)
+{
+    TVector<N> result;
+    for (size_t i = 0; i < N; ++i)
+        result[i] = a[i] * b[i];
+    return result;
+}
+
+template <size_t N>
+TVector<N> operator + (const TVector<N>& a, float b)
+{
+    TVector<N> result;
+    for (size_t i = 0; i < N; ++i)
+        result[i] = a[i] + b;
+    return result;
+}
+
+template <size_t N>
+TVector<N> operator * (const TVector<N>& a, float b)
+{
+    TVector<N> result;
+    for (size_t i = 0; i < N; ++i)
+        result[i] = a[i] * b;
+    return result;
+}
+
+template <size_t N>
+TVector<N> operator / (const TVector<N>& a, float b)
+{
+    TVector<N> result;
+    for (size_t i = 0; i < N; ++i)
+        result[i] = a[i] / b;
+    return result;
+}
+
+template <size_t N>
+float LenSquared (const TVector<N>& a)
+{
+    float length = 0.0f;
+    for (size_t i = 0; i < N; ++i)
+        length += a[i] * a[i];
+    return length;
+}
+
+template <size_t N>
+float Len (const TVector<N>& a)
+{
+    return std::sqrt(LenSquared(a));
+}
+
+template <size_t N>
+void Normalize (const TVector<N>& a)
+{
+    a = a / Len(a);
+}
+
+template <size_t N>
+size_t LargestMagnitudeComponent (const TVector<N>& a)
+{
+    size_t winningIndex = 0;
+    for (size_t i = 1; i < N; ++i)
+    {
+        if (std::abs(a[i]) > std::abs(a[winningIndex]))
+            winningIndex = i;
+    }
+    return winningIndex;
+}
+
+// ==================================================================================================================
+template <typename T>
+T Clamp (T value, T min, T max)
+{
+    if (value < min)
+        return min;
+    else if (value > max)
+        return max;
+    else
+        return value;
+}
+
+// ==================================================================================================================
+ struct SImageData
 {
     SImageData()
         : m_width(0)
@@ -29,16 +127,19 @@ struct SImageData
     std::vector<uint8> m_pixels;
 };
 
+// ==================================================================================================================
 SImageData g_srcImages[6];
 SImageData g_destImages[6];
-  
+
+// ==================================================================================================================
 void WaitForEnter ()
 {
     printf("Press Enter to quit");
     fflush(stdin);
     getchar();
 }
-  
+
+// ==================================================================================================================
 bool LoadImage (const char *fileName, SImageData& imageData)
 {
     // open the file if we can
@@ -74,7 +175,8 @@ bool LoadImage (const char *fileName, SImageData& imageData)
     fclose(file);
     return true;
 }
-  
+
+// ==================================================================================================================
 bool SaveImage (const char *fileName, const SImageData &image)
 {
     // open the file if we can
@@ -114,9 +216,73 @@ bool SaveImage (const char *fileName, const SImageData &image)
     return true;
 }
 
+// ==================================================================================================================
+TVector3 SampleSourceCubeMap (const TVector3& direction)
+{
+    size_t largestComponent = LargestMagnitudeComponent(direction);
+    bool negFace = direction[largestComponent] < 0.0f;
+    TVector3 cubePos = direction / direction[largestComponent];
+    if (negFace)
+        cubePos = cubePos * -1.0f;
+
+    size_t faceIndex = largestComponent;
+    if (!negFace)
+        faceIndex += 3;
+
+    // TODO: can we combine this somehow with the u/v axis calculations in ProcessFace?
+    TVector2 uv = { 0.0f, 0.0f };
+    switch (largestComponent)
+    {
+        case 0:
+        {
+            uv[0] = cubePos[2] * (negFace ? -1.0f : 1.0f);
+            uv[1] = cubePos[1];
+            break;
+        }
+        case 1:
+        {
+            uv[0] = cubePos[0];
+            uv[1] = cubePos[2] * (negFace ? -1.0f : 1.0f);
+            break;
+        }
+        case 2:
+        {
+            uv[0] = cubePos[0] * (negFace ? -1.0f : 1.0f);
+            uv[1] = cubePos[1];
+            break;
+        }
+    }
+    uv = uv * 0.5f + 0.5f;
+
+    // TODO: this clamping thing could be done better.
+    size_t pixelX = (size_t)(float(g_srcImages[faceIndex].m_width-1) * uv[0]);
+    pixelX = Clamp<size_t>(pixelX, 0, g_srcImages[faceIndex].m_width - 1);
+
+    size_t pixelY = (size_t)(float(g_srcImages[faceIndex].m_height-1) * uv[1]);
+    pixelY = Clamp<size_t>(pixelY, 0, g_srcImages[faceIndex].m_height - 1);
+
+    size_t pixelOffset = pixelY * g_srcImages[faceIndex].m_pitch + pixelX * 3;
+
+    return
+    {
+        float(g_srcImages[faceIndex].m_pixels[pixelOffset + 0]) / 255.0f,
+        float(g_srcImages[faceIndex].m_pixels[pixelOffset + 1]) / 255.0f,
+        float(g_srcImages[faceIndex].m_pixels[pixelOffset + 2]) / 255.0f,
+    };
+
+    // TODO: bilinear interpolation!
+}
+
+// ==================================================================================================================
+TVector3 DiffuseIrradianceForNormal (const TVector3& normal)
+{
+    // TODO: sample over hemisphere etc
+    return SampleSourceCubeMap(normal);
+}
+
+// ==================================================================================================================
 void ProcessFace (size_t faceIndex)
 {
-    // TODO: this function
     TVector3 facePlane = { 0, 0, 0 };
     facePlane[faceIndex % 3] = (faceIndex / 3) ? 1.0f : -1.0f;
     TVector3 uAxis = { 0, 0, 0 };
@@ -145,26 +311,30 @@ void ProcessFace (size_t faceIndex)
     SImageData &destData = g_destImages[faceIndex];
     for (size_t iy = 0; iy < destData.m_height; ++iy)
     {
+        uint8* pixel = &destData.m_pixels[iy * destData.m_pitch];
+
         TVector2 uv;
         uv[1] = (float(iy) / float(destData.m_height - 1));
         for (size_t ix = 0; ix < destData.m_width; ++ix)
         {
             uv[0] = (float(ix) / float(destData.m_width - 1));
 
-            // TOOD: call ProcessNormal() to process a single pixel?
-
             // calculate the position of the pixel on the cube and normalize it to get the normal to process
-            /*
-            TVector3 pixelPosition =
+            TVector3 normalDir =
                 facePlane +
                 uAxis * (uv[0] * 2.0f - 1.0f) +
-                vaxis + (uv[1] * 2.0f - 1.0f);
-            Normalize(pixelPosition);
-            TVector3 irradiance = ProcessNormal(pixelPosition);
-                */
+                vAxis * (uv[1] * 2.0f - 1.0f);
+
+            TVector3 diffuseIrradiance = DiffuseIrradianceForNormal(normalDir);
+
+            pixel[0] = (uint8)Clamp(diffuseIrradiance[0] * 255.0f, 0.0f, 255.0f);
+            pixel[1] = (uint8)Clamp(diffuseIrradiance[1] * 255.0f, 0.0f, 255.0f);
+            pixel[2] = (uint8)Clamp(diffuseIrradiance[2] * 255.0f, 0.0f, 255.0f);
+            pixel += 3;
 
             // TODO: put irradiance into destination image.
             // TODO: what if it's too bright. Clip for now? Or normalize the result? maybe normalize across all images?
+            // TODO: maybe have these processes write to floating point values, then have a post process to normalize, and another to shrink down to desired size
         }
     }
 
@@ -182,7 +352,8 @@ void ThreadFunc ()
         faceIndex = s_faceIndex.fetch_add(1);
     }
 }
- 
+
+// ==================================================================================================================
 int main (int argc, char **argv)
 {
     // TODO: take from command line
@@ -266,6 +437,7 @@ int main (int argc, char **argv)
 /*
 
 TODO:
+* the skybox copy is "working", but the edges of the output images have some problems
 * take in a skybox, do convolution against cos theta, output the resulting skybox
 * should we make it multithreaded? spawn up a thread per face? it only needs read only data of the source images so maybe!
 * profile if needed and find slow parts
@@ -273,6 +445,7 @@ TODO:
 * i guess the result can be small.  The tutorial says 32x32?  Do you generate and then shrink? Or do you make small to begin with?
 * test 32 and 64 bit mode
 ? would it be better to use cosine weighted samples or anything?
+* we may not need the normalize() function (or length / length squared then!)
 
 Blog:
 * Link to PBR / IBL tutorial: https://learnopengl.com/#!PBR/IBL/Diffuse-irradiance
