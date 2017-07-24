c6f9d2e576960fcd62960dc9955d083c877b3580
Alan Wolfe <alan.wolfe@gmail.com>
2017-07-24 12:31:33 -0700

    Code nearly done!

--- NormalDistribution/Source.cpp
+++ NormalDistribution/Source.cpp
@@ -18,13 +18,21 @@ void DumpBucketCountsAddRandomNumbers (size_t numSamples, const std::array<size_
         return;
 
     // write the info
+    float mean = float(DiceCount) * float(DiceRange - 1.0f) / 2.0f;
+    float variance = float(DiceCount) * (DiceRange * DiceRange) / 12.0f;
     if (numSamples == 1)
-        fprintf(file, "\"%zu random numbers [0,%zu) added together (sum %zud%zu). %zu buckets.\"\n", DiceCount, DiceRange, DiceCount, DiceRange, NumBuckets);
+    {
+        fprintf(file, "\"%zu random numbers [0,%zu) added together (sum %zud%zu). %zu buckets.  Mean = %0.2f.  Variance = %0.2f.  StdDev = %0.2f.\"\n", DiceCount, DiceRange, DiceCount, DiceRange, NumBuckets, mean, variance, std::sqrt(variance));
+        fprintf(file, "\"\"");
+        for (size_t i = 0; i < NumBuckets; ++i)
+            fprintf(file, ",\"%zu\"", i);
+        fprintf(file, "\n");
+    }
     fprintf(file, "\"%zu samples\",", numSamples);
 
-    // report the samples such that they integrate to 1.0
+    // report the samples
     for (size_t count : bucketCounts)
-        fprintf(file, "\"%f\",", float(count) / float(numSamples));
+        fprintf(file, "\"%zu\",", count);
 
     fprintf(file, "\"\"\n");
     if (numSamples == c_maxNumSamples)
@@ -69,13 +77,21 @@ void DumpBucketCountsCountBits (size_t numSamples, const std::array<size_t, NumB
         return;
 
     // write the info
+    float mean = float(NumBuckets-1) * 1.0f / 2.0f;
+    float variance = float(NumBuckets-1) * 3.0f / 12.0f;
     if (numSamples == 1)
-        fprintf(file, "\"%zu random bits (coin flips) added together. %zu buckets.\"\n", NumBuckets - 1, NumBuckets);
+    {
+        fprintf(file, "\"%zu random bits (coin flips) added together. %zu buckets.  Mean = %0.2f.  Variance = %0.2f.  StdDev = %0.2f.\"\n", NumBuckets - 1, NumBuckets, mean, variance, std::sqrt(variance));
+        fprintf(file, "\"\"");
+        for (size_t i = 0; i < NumBuckets; ++i)
+            fprintf(file, ",\"%zu\"", i);
+        fprintf(file, "\n");
+    }
     fprintf(file, "\"%zu samples\",", numSamples);
 
-    // report the samples such that they integrate to 1.0
+    // report the samples
     for (size_t count : bucketCounts)
-        fprintf(file, "\"%f\",", float(count) / float(numSamples));
+        fprintf(file, "\"%zu\",", count);
 
     fprintf(file, "\"\"\n");
     if (numSamples == c_maxNumSamples)
@@ -121,6 +137,35 @@ void CountBitsTest ()
     }
 }
 
+float GenerateNormalRandomNumber (float mean, float variance)
+{
+    static std::mt19937 rng;
+    static std::uniform_int_distribution<uint64_t> dist(0, (uint64_t)-1);
+
+    static bool seeded = false;
+    if (!seeded)
+        rng.seed(std::random_device()());
+
+    // generate our normal distributed random number from 0 to 65.
+    // mean = 32, variance = 16, stddev = 4
+    float sum = 0.0f;
+    uint64_t number = dist(rng);
+    while (number)
+    {
+        if (number & 1)
+            sum += 1.0f;
+        number = number >> 1;
+    }
+
+    // convert to the specified mean and variance
+    float ret = sum;
+    ret -= 32.0f;
+    ret /= 4.0f;
+    ret *= std::sqrt(variance);
+    ret += mean;
+    return ret;
+}
+
 int main (int argc, char **argv)
 {
     // clear out the file
@@ -149,34 +194,37 @@ int main (int argc, char **argv)
 
 
 /*
+* somehow show that the GenerateNormalRandomNumber function is right.  Not sure how.
+ * maybe spit out a bunch of values and make a histogram that can be graphed, like the others?
 
-* grab notes from email and put in here.
-
-! make a function for gaussian distribution. RNG a uint64, count bits, divide by 64?
- * should you also prove it's right?
+BLOG:
 
 ? which tests specifically would be good to do and show for blog post?
- 
+ ? show difference in increasing dice side vs dice count?
+ * mean is (n-1)/2.  Or if 1 based: (n+1)/2.
+ * variance is (n*n-1)/12.
+ * those are multiplied by the number of dice summed.
+
+* two names: normal / gaussian distribution
+
+* mention std function for gaussian distribution
+ * and an: http://www.design.caltech.edu/erik/Misc/Gaussian.html
+
+* Also, make your code / explain how to make distributions with different means and variance squared
+
+* converting one distribution to another: https://stats.stackexchange.com/questions/43114/how-to-convert-to-gaussian-distribution-with-given-mean-and-standard-deviation/43117#43117
+
+john cook links that inspired this:
+* https://www.johndcook.com/blog/2015/03/09/why-isnt-everything-normally-distributed/
+* https://www.johndcook.com/blog/2009/09/29/achievement-is-log-normal/
+
+
+* irrelevant?
 * i think your normalization is still wrong. wikipedia shows regular old std dev with a peak of 0.4.  Yours isn't / doesn't. Why?
  * you aren't taking into account the width of your bins when normalizing it to integrate it to 1.0?
  * https://math.stackexchange.com/questions/468541/normalizing-a-gaussian-distribution
  * +/- 3 stddeviations is 99.7%.  Does it ever reach 100%? If not, maybe saying the graph has width 6 overall is enough to make it normalize (to integrate to 1.0) correctly?
  * you need to understand this and explain it in the blog post
 
-
-? better name for variables than Numbers, NumSamples, SampleCount?
-
-* Also the thing about counting number of bits in a random number to make gaussian distribution.
- * They are related because you are adding random 1 bit numbers!
-
-* With both methods: there are that many ways to make that total.
-
-* Can we generate log normal distribution?
-
-* two dimensional distribution, like gaussian kernel?
- * could do with a tensor product but explain how it relates to "there are that manyt ways to make that total"?
-
-BLOG:
-* two names: normal / gaussian distribution
 
 */
