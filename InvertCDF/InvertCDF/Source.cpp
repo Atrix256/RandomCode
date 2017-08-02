1c6e3ae034eb07323d5b17c1467343771e360774
Alan Wolfe <alan.wolfe@gmail.com>
2017-08-02 13:37:57 -0700

    blargh

--- InvertCDF/InvertCDF/Source.cpp
+++ InvertCDF/InvertCDF/Source.cpp
@@ -5,13 +5,15 @@ int main (int argc, char **argv)
 {
     FILE *file = fopen("blah.csv", "w+t");
 
-    std::mt19937 rng;
-    rng.seed(std::random_device()());
-    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_real_distribution<float> dist(0.0f, std::nextafter(1.0f, std::numeric_limits<float>::max()));
 
-    for (size_t i = 0; i < 1000; ++i)
+    for (size_t i = 0; i < 100000; ++i)
     {
-        fprintf(file, "\"%f\"\n", std::sqrt(dist(rng)));
+        float x = dist(rng);
+        float y = std::sqrt(x);
+        fprintf(file, "%f,%f\n", x, y);
     }
 
     fclose(file);
