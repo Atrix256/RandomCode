608626332f02d85504fd2f30f64a48e128128d01
Atrix256 <alan.wolfe@gmail.com>
2017-08-06 21:05:41 -0700

    much progress on rejection sampling

--- RejectionSampling/Source.cpp
+++ RejectionSampling/Source.cpp
@@ -4,6 +4,37 @@
 #include <random>
 #include <array>
 #include <unordered_map>
+
+template <size_t NUM_TEST_SAMPLES, size_t SIMULATED_DICE_SIDES, size_t ACTUAL_DICE_SIDES>
+void TestDice (const char* fileName)
+{
+    // seed the random number generator
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<size_t> dist(0, ACTUAL_DICE_SIDES);
+
+    // generate the histogram
+    std::array<size_t, SIMULATED_DICE_SIDES> histogram = { 0 };
+    size_t rejectedSamples = 0;
+    for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
+    {
+        size_t roll = dist(rng);
+        while (roll >= SIMULATED_DICE_SIDES)
+        {
+            ++rejectedSamples;
+            roll = dist(rng);
+        }
+        histogram[roll]++;
+    }
+
+    // write the histogram and rejected sample count to a csv
+    // an extra 0 data point forces the graph to include 0 in the scale. hack to make the data not look noisier than it really is.
+    FILE *file = fopen(fileName, "w+t");
+    fprintf(file, "Actual Count, Expected Count, Dummy Data, %0.2f samples needed per roll on average.\n", (float(NUM_TEST_SAMPLES) + float(rejectedSamples)) / float(NUM_TEST_SAMPLES));
+    for (size_t value : histogram)
+        fprintf(file, "%zu,%zu,0\n", value, (size_t)(float(NUM_TEST_SAMPLES) / float(SIMULATED_DICE_SIDES)));
+    fclose(file);
+}
  
 template <size_t NUM_TEST_SAMPLES, size_t NUM_HISTOGRAM_BUCKETS, typename PDF_LAMBDA>
 void Test (const char* fileName, float maxPDFValue, const PDF_LAMBDA& PDF)
@@ -16,6 +47,7 @@ void Test (const char* fileName, float maxPDFValue, const PDF_LAMBDA& PDF)
     // generate the histogram
     std::array<size_t, NUM_HISTOGRAM_BUCKETS> histogram = { 0 };
     std::array<size_t, NUM_HISTOGRAM_BUCKETS> testCounts = { 0 };
+    size_t rejectedSamples = 0;
     for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
     {
         // Generate a sample from the PDF by generating a random 2d point.
@@ -36,6 +68,7 @@ void Test (const char* fileName, float maxPDFValue, const PDF_LAMBDA& PDF)
             {
                 size_t bin = (size_t)std::floor(pointX * float(NUM_HISTOGRAM_BUCKETS));
                 testCounts[std::min(bin, NUM_HISTOGRAM_BUCKETS - 1)]++;
+                ++rejectedSamples;
             }
         }
  
@@ -46,14 +79,74 @@ void Test (const char* fileName, float maxPDFValue, const PDF_LAMBDA& PDF)
  
     // write the histogram and pdf sample to a csv
     FILE *file = fopen(fileName, "w+t");
-    fprintf(file, "PDF, Inverted CDF, Failed Tests\n");
+    fprintf(file, "PDF, Simulated PDF, Generating Function, Failed Tests, %0.2f samples needed per value on average.\n", (float(NUM_TEST_SAMPLES) + float(rejectedSamples)) / float(NUM_TEST_SAMPLES));
     for (size_t i = 0; i < NUM_HISTOGRAM_BUCKETS; ++i)
     {
         float x = (float(i) + 0.5f) / float(NUM_HISTOGRAM_BUCKETS);
         float pdfSample = PDF(x);
-        fprintf(file, "%f,%f,%f\n",
+        fprintf(file, "%f,%f,%f,%f\n",
             pdfSample,
             NUM_HISTOGRAM_BUCKETS * float(histogram[i]) / float(NUM_TEST_SAMPLES),
+            maxPDFValue,
+            float(testCounts[i])
+        );
+    }
+    fclose(file);
+}
+
+template <size_t NUM_TEST_SAMPLES, size_t NUM_HISTOGRAM_BUCKETS, typename PDF_LAMBDA>
+void TestNotPDF (const char* fileName, float maxPDFValue, float normalizationConstant, const PDF_LAMBDA& PDF)
+{
+    // seed the random number generator
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+ 
+    // generate the histogram
+    std::array<size_t, NUM_HISTOGRAM_BUCKETS> histogram = { 0 };
+    std::array<size_t, NUM_HISTOGRAM_BUCKETS> testCounts = { 0 };
+    size_t rejectedSamples = 0;
+    for (size_t i = 0; i < NUM_TEST_SAMPLES; ++i)
+    {
+        // Generate a sample from the PDF by generating a random 2d point.
+        // If the y axis of the value is <= the value returned by PDF(x), accept it, else reject it.
+        // NOTE: this takes an unknown number of iterations, and technically may NEVER finish.
+        float pointX = 0.0f;
+        float pointY = 0.0f;
+        bool validPoint = false;
+        while (!validPoint)
+        {
+            pointX = dist(rng);
+            pointY = dist(rng) * maxPDFValue;
+            float pdfValue = PDF(pointX);
+            validPoint = (pointY <= pdfValue);
+
+            // track number of failed tests per histogram bucket
+            if (!validPoint)
+            {
+                size_t bin = (size_t)std::floor(pointX * float(NUM_HISTOGRAM_BUCKETS));
+                testCounts[std::min(bin, NUM_HISTOGRAM_BUCKETS - 1)]++;
+                ++rejectedSamples;
+            }
+        }
+ 
+        // increment the correct bin in the histogram
+        size_t bin = (size_t)std::floor(pointX * float(NUM_HISTOGRAM_BUCKETS));
+        histogram[std::min(bin, NUM_HISTOGRAM_BUCKETS -1)]++;
+    }
+ 
+    // write the histogram and pdf sample to a csv
+    FILE *file = fopen(fileName, "w+t");
+    fprintf(file, "Function, Simulated PDF, Scaled Simulated PDF, Generating Function, Failed Tests, %0.2f samples needed per value on average.\n", (float(NUM_TEST_SAMPLES) + float(rejectedSamples)) / float(NUM_TEST_SAMPLES));
+    for (size_t i = 0; i < NUM_HISTOGRAM_BUCKETS; ++i)
+    {
+        float x = (float(i) + 0.5f) / float(NUM_HISTOGRAM_BUCKETS);
+        float pdfSample = PDF(x);
+        fprintf(file, "%f,%f,%f,%f,%f\n",
+            pdfSample,
+            NUM_HISTOGRAM_BUCKETS * float(histogram[i]) / float(NUM_TEST_SAMPLES),
+            NUM_HISTOGRAM_BUCKETS * float(histogram[i]) / float(NUM_TEST_SAMPLES) * normalizationConstant,
+            maxPDFValue,
             float(testCounts[i])
         );
     }
@@ -62,55 +155,50 @@ void Test (const char* fileName, float maxPDFValue, const PDF_LAMBDA& PDF)
  
 int main (int argc, char **argv)
 {
-    // PDF: y=2x
+    // Dice
+    {
+        // Simulate a 5 sided dice with a 6 sided dice
+        TestDice<10000, 5, 6>("test1_5_6.csv");
+
+        // Simulate a 5 sided dice with a 20 sided dice
+        TestDice<10000, 5, 20>("test1_5_20.csv");
+    }
+
+    // PDF y=2x, simulated with a uniform distribution
     {
         auto PDF = [](float x) { return 2.0f * x; };
 
-        Test<1000, 100>("test1_1k.csv", 2.0f, PDF);
-        Test<100000, 100>("test1_100k.csv", 2.0f, PDF);
-        Test<1000000, 100>("test1_1m.csv", 2.0f, PDF);
+        Test<1000, 100>("test2_1k.csv", 2.0f, PDF);
+        Test<100000, 100>("test2_100k.csv", 2.0f, PDF);
+        Test<1000000, 100>("test2_1m.csv", 2.0f, PDF);
     }
 
-    /*
-    // PDF: y=2x
-    // inverse CDF: y=sqrt(x)
+    // PDF y=(x^3-10x^2+5x+11)/10.417, simulated with a uniform distribution
     {
-        auto PDF = [] (float x) { return 2.0f * x; };
-        auto inverseCDF = [] (float x) { return std::sqrt(x); };
- 
-        Test<1000, 100>("test1_1k.csv", PDF, inverseCDF);
-        Test<100000, 100>("test1_100k.csv", PDF, inverseCDF);
-        Test<1000000, 100>("test1_1m.csv", PDF, inverseCDF);
+        auto PDF = [](float x) {return (x*x*x - 10.0f*x*x + 5.0f*x + 11.0f) / (10.417f); };
+        Test<10000000, 100>("test3_10m_1_1.csv", 1.1f, PDF);
+        Test<10000000, 100>("test3_10m_1_2.csv", 1.2f, PDF);
+        Test<10000000, 100>("test3_10m_2_8.csv", 2.8f, PDF);
+    }
+
+    // function (not PDF, Doesn't integrate to 1!) y=(x^3-10x^2+5x+11), simulated with a uniform distribution
+    {
+        auto PDF = [](float x) {return (x*x*x - 10.0f*x*x + 5.0f*x + 11.0f); };
+        TestNotPDF<10000000, 100>("test4_10m_12_5.csv", 12.5f, 10.417f, PDF);
     }
  
-    // PDF: y=3x^2
-    // inverse CDF: y=cuberoot(x) aka y = pow(x, 1/3)
-    {
-        auto PDF = [] (float x) { return 3.0f * x * x; };
-        auto inverseCDF = [](float x) { return std::pow(x, 1.0f / 3.0f); };
- 
-        Test<100000, 100>("test2_100k.csv", PDF, inverseCDF);
-    }
- 
-    // PDF: y=(x^3-10x^2+5x+11)/10.417
-    // Inverse CDF Numerically via a lookup table
-    {
-        auto PDF = [] (float x) {return (x*x*x - 10.0f*x*x + 5.0f*x + 11.0f) / (10.417f); };
-        TestPDFOnly<100000000, 100, 3>("test3_100m_3.csv", PDF);
-        TestPDFOnly<100000000, 100, 5>("test3_100m_5.csv", PDF);
-        TestPDFOnly<100000000, 100, 10>("test3_100m_10.csv", PDF);
-        TestPDFOnly<100000000, 100, 25>("test3_100m_25.csv", PDF);
-        TestPDFOnly<100000000, 100, 25>("test3_100m_100.csv", PDF);
-    }
-    */
- 
+    // TODO: generate a distribution using a non uniform distribution!
+
     return 0;
 }
 /*
 
 TODO:
-* Simple version: uniform random numbers
-* More complex version: use a different distribution to generate a third distribution!  maybe use gaussian to generate an exponential or something
+
+Blog:
+? can you calculate how many rolls should be needed on average for simulating dice? put that in blog post
+ * yes. If the thing is a PDF it has area of 1.0.  If the generating distribution has area of X, X is how many samples you need. totally logical and simple
+* also note, the PDF just always needs to be greater than the other distribution. If you don't know maximum value thats fine, just choose one that is for sure greater (round up a bit).
 
 ARS:
 Idea: when rejection sampling you want to sample something that has very little wasted space so you throw away fewer samples.
