4bc61a5ed725e129abfe17c6bc209f95684235b2
Alan Wolfe <alan.wolfe@gmail.com>
2017-10-23 13:24:51 -0700

    inverted checkin... doh!

--- NoiseShaping/Source.cpp
+++ NoiseShaping/Source.cpp
@@ -5,9 +5,21 @@
 #include <vector>
 #include <random>
 #include <array>
+#include <thread>
+#include <complex>
+#include <atomic>
 
 typedef uint8_t uint8;
 typedef int64_t int64;
+
+const float c_pi = 3.14159265359f;
+
+// settings
+const size_t    c_imageSize = 256;
+const bool      c_doDFT = true; // TODO: true before checkin!
+const float     c_blurSigma = 1.0f;
+const float     c_blurThresholdPercent = 0.005f; // lower numbers give higher quality results, but take longer. This is 0.5%
+const float     c_numBlurs = 5;
 
 //======================================================================================
 struct SImageData
@@ -22,8 +34,6 @@ struct SImageData
     size_t m_pitch;
     std::vector<uint8> m_pixels;
 };
-
-SImageData s_stippleImage;
  
 //======================================================================================
 struct SColor
@@ -41,6 +51,170 @@ struct SColor
  
     uint8 B, G, R;
 };
+
+//======================================================================================
+struct SImageDataFloat
+{
+    SImageDataFloat()
+        : m_width(0)
+        , m_height(0)
+    { }
+   
+    size_t m_width;
+    size_t m_height;
+    std::vector<float> m_pixels;
+};
+
+//======================================================================================
+struct SImageDataComplex
+{
+    SImageDataComplex ()
+        : m_width(0)
+        , m_height(0)
+    { }
+  
+    size_t m_width;
+    size_t m_height;
+    std::vector<std::complex<float>> m_pixels;
+};
+ 
+//======================================================================================
+std::complex<float> DFTPixel (const SImageData &srcImage, size_t K, size_t L)
+{
+    std::complex<float> ret(0.0f, 0.0f);
+  
+    for (size_t x = 0; x < srcImage.m_width; ++x)
+    {
+        for (size_t y = 0; y < srcImage.m_height; ++y)
+        {
+            // Get the pixel value (assuming greyscale) and convert it to [0,1] space
+            const uint8 *src = &srcImage.m_pixels[(y * srcImage.m_pitch) + x * 3];
+            float grey = float(src[0]) / 255.0f;
+  
+            // Add to the sum of the return value
+            float v = float(K * x) / float(srcImage.m_width);
+            v += float(L * y) / float(srcImage.m_height);
+            ret += std::complex<float>(grey, 0.0f) * std::polar<float>(1.0f, -2.0f * c_pi * v);
+        }
+    }
+  
+    return ret;
+}
+  
+//======================================================================================
+void ImageDFT (const SImageData &srcImage, SImageDataComplex &destImage)
+{
+    // NOTE: this function assumes srcImage is greyscale, so works on only the red component of srcImage.
+    // ImageToGrey() will convert an image to greyscale.
+ 
+    // size the output dft data
+    destImage.m_width = srcImage.m_width;
+    destImage.m_height = srcImage.m_height;
+    destImage.m_pixels.resize(destImage.m_width*destImage.m_height);
+ 
+    size_t numThreads = std::thread::hardware_concurrency();
+    //if (numThreads > 0)
+        //numThreads = numThreads - 1;
+ 
+    std::vector<std::thread> threads;
+    threads.resize(numThreads);
+ 
+    printf("Doing DFT with %zu threads...\n", numThreads);
+ 
+    // calculate 2d dft (brute force, not using fast fourier transform) multithreadedly
+    std::atomic<size_t> nextRow(0);
+    for (std::thread& t : threads)
+    {
+        t = std::thread(
+            [&] ()
+            {
+                size_t row = nextRow.fetch_add(1);
+                bool reportProgress = (row == 0);
+                int lastPercent = -1;
+ 
+                while (row < srcImage.m_height)
+                {
+                    // calculate the DFT for every pixel / frequency in this row
+                    for (size_t x = 0; x < srcImage.m_width; ++x)
+                    {
+                        destImage.m_pixels[row * destImage.m_width + x] = DFTPixel(srcImage, x, row);
+                    }
+ 
+                    // report progress if we should
+                    if (reportProgress)
+                    {
+                        int percent = int(100.0f * float(row) / float(srcImage.m_height));
+                        if (lastPercent != percent)
+                        {
+                            lastPercent = percent;
+                            printf("            \rDFT: %i%%", lastPercent);
+                        }
+                    }
+ 
+                    // go to the next row
+                    row = nextRow.fetch_add(1);
+                }
+            }
+        );
+    }
+ 
+    for (std::thread& t : threads)
+        t.join();
+ 
+    printf("\n");
+}
+ 
+//======================================================================================
+void GetMagnitudeData (const SImageDataComplex& srcImage, SImageData& destImage)
+{
+    // size the output image
+    destImage.m_width = srcImage.m_width;
+    destImage.m_height = srcImage.m_height;
+    destImage.m_pitch = 4 * ((srcImage.m_width * 24 + 31) / 32);
+    destImage.m_pixels.resize(destImage.m_pitch*destImage.m_height);
+  
+    // get floating point magnitude data
+    std::vector<float> magArray;
+    magArray.resize(srcImage.m_width*srcImage.m_height);
+    float maxmag = 0.0f;
+    for (size_t x = 0; x < srcImage.m_width; ++x)
+    {
+        for (size_t y = 0; y < srcImage.m_height; ++y)
+        {
+            // Offset the information by half width & height in the positive direction.
+            // This makes frequency 0 (DC) be at the image origin, like most diagrams show it.
+            int k = (x + (int)srcImage.m_width / 2) % (int)srcImage.m_width;
+            int l = (y + (int)srcImage.m_height / 2) % (int)srcImage.m_height;
+            const std::complex<float> &src = srcImage.m_pixels[l*srcImage.m_width + k];
+  
+            float mag = std::abs(src);
+            if (mag > maxmag)
+                maxmag = mag;
+  
+            magArray[y*srcImage.m_width + x] = mag;
+        }
+    }
+    if (maxmag == 0.0f)
+        maxmag = 1.0f;
+  
+    const float c = 255.0f / log(1.0f+maxmag);
+  
+    // normalize the magnitude data and send it back in [0, 255]
+    for (size_t x = 0; x < srcImage.m_width; ++x)
+    {
+        for (size_t y = 0; y < srcImage.m_height; ++y)
+        {
+            float src = c * log(1.0f + magArray[y*srcImage.m_width + x]);
+  
+            uint8 magu8 = uint8(src);
+  
+            uint8* dest = &destImage.m_pixels[y*destImage.m_pitch + x * 3];
+            dest[0] = magu8;
+            dest[1] = magu8;
+            dest[2] = magu8;
+        }
+    }
+}
 
 //======================================================================================
 bool ImageSave (const SImageData &image, const char *fileName)
@@ -128,20 +302,154 @@ void ImageInit (SImageData& image, size_t width, size_t height)
     image.m_width = width;
     image.m_height = height;
     image.m_pitch = 4 * ((width * 24 + 31) / 32);
-    image.m_pixels.resize(image.m_pitch * image.m_width);
+    image.m_pixels.resize(image.m_pitch * image.m_height);
     std::fill(image.m_pixels.begin(), image.m_pixels.end(), 0);
 }
 
 //======================================================================================
-template <size_t N>
-void SaveFloatArrayAsBMP (const std::array<float, N>& data, const char* fileName, size_t imageSize)
+void ImageFloatInit (SImageDataFloat& image, size_t width, size_t height)
+{
+    image.m_width = width;
+    image.m_height = height;
+    image.m_pixels.resize(image.m_width * image.m_height);
+    std::fill(image.m_pixels.begin(), image.m_pixels.end(), 0.0f);
+}
+
+//======================================================================================
+int PixelsNeededForSigma (float sigma)
+{
+    // returns the number of pixels needed to represent a gaussian kernal that has values
+    // down to the threshold amount.  A gaussian function technically has values everywhere
+    // on the image, but the threshold lets us cut it off where the pixels contribute to
+    // only small amounts that aren't as noticeable.
+    return int(floor(1.0f + 2.0f * sqrtf(-2.0f * sigma * sigma * log(c_blurThresholdPercent)))) + 1;
+}
+
+//======================================================================================
+float Gaussian (float sigma, float x)
+{
+    return expf(-(x*x) / (2.0f * sigma*sigma));
+}
+
+//======================================================================================
+float GaussianSimpsonIntegration (float sigma, float a, float b)
+{
+    return
+        ((b - a) / 6.0f) *
+        (Gaussian(sigma, a) + 4.0f * Gaussian(sigma, (a + b) / 2.0f) + Gaussian(sigma, b));
+}
+
+//======================================================================================
+std::vector<float> GaussianKernelIntegrals (float sigma, int taps)
+{
+    std::vector<float> ret;
+    float total = 0.0f;
+    for (int i = 0; i < taps; ++i)
+    {
+        float x = float(i) - float(taps / 2);
+        float value = GaussianSimpsonIntegration(sigma, x - 0.5f, x + 0.5f);
+        ret.push_back(value);
+        total += value;
+    }
+    // normalize it
+    for (unsigned int i = 0; i < ret.size(); ++i)
+    {
+        ret[i] /= total;
+    }
+    return ret;
+}
+
+//======================================================================================
+const float* GetPixelWrapAround (const SImageDataFloat& image, int x, int y)
+{
+    if (x >= (int)image.m_width)
+    {
+        x = x % (int)image.m_width;
+    }
+    else
+    {
+        while (x < 0)
+            x += (int)image.m_width;
+    }
+
+    if (y >= (int)image.m_height)
+    {
+        y = y % (int)image.m_height;
+    }
+    else
+    {
+        while (y < 0)
+            y += (int)image.m_height;
+    }
+
+    return &image.m_pixels[(y * image.m_width) + x];
+}
+
+//======================================================================================
+void ImageGaussianBlur (const SImageDataFloat& srcImage, SImageDataFloat &destImage, float xblursigma, float yblursigma, unsigned int xblursize, unsigned int yblursize)
+{
+    // allocate space for copying the image for destImage and tmpImage
+    ImageFloatInit(destImage, srcImage.m_width, srcImage.m_height);
+ 
+    SImageDataFloat tmpImage;
+    ImageFloatInit(tmpImage, srcImage.m_width, srcImage.m_height);
+ 
+    // horizontal blur from srcImage into tmpImage
+    {
+        auto row = GaussianKernelIntegrals(xblursigma, xblursize);
+ 
+        int startOffset = -1 * int(row.size() / 2);
+ 
+        for (int y = 0; y < tmpImage.m_height; ++y)
+        {
+            for (int x = 0; x < tmpImage.m_width; ++x)
+            {
+                float blurredPixel = 0.0f;
+                for (unsigned int i = 0; i < row.size(); ++i)
+                {
+                    const float *pixel = GetPixelWrapAround(srcImage, x + startOffset + i, y);
+                    blurredPixel += pixel[0] * row[i];
+                }
+                 
+                float *destPixel = &tmpImage.m_pixels[y * tmpImage.m_width + x];
+                destPixel[0] = blurredPixel;
+            }
+        }
+    }
+ 
+    // vertical blur from tmpImage into destImage
+    {
+        auto row = GaussianKernelIntegrals(yblursigma, yblursize);
+ 
+        int startOffset = -1 * int(row.size() / 2);
+ 
+        for (int y = 0; y < destImage.m_height; ++y)
+        {
+            for (int x = 0; x < destImage.m_width; ++x)
+            {
+                float blurredPixel = 0.0f;
+                for (unsigned int i = 0; i < row.size(); ++i)
+                {
+                    const float *pixel = GetPixelWrapAround(tmpImage, x, y + startOffset + i);
+                    blurredPixel += pixel[0] * row[i];
+                }
+ 
+                float *destPixel = &destImage.m_pixels[y * destImage.m_width + x];
+                destPixel[0] = blurredPixel;
+            }
+        }
+    }
+}
+
+//======================================================================================
+void SaveImageFloatAsBMP (const SImageDataFloat& imageFloat, const char* fileName)
 {
     // init the image
     SImageData image;
-    ImageInit(image, imageSize, imageSize);
+    ImageInit(image, imageFloat.m_width, imageFloat.m_height);
 
     // write the data to the image
-    const float* srcData = &data[0];
+    const float* srcData = &imageFloat.m_pixels[0];
     for (size_t y = 0; y < image.m_height; ++y)
     {
         SColor* pixel = (SColor*)&image.m_pixels[y*image.m_pitch];
@@ -159,31 +467,149 @@ void SaveFloatArrayAsBMP (const std::array<float, N>& data, const char* fileName
 
     // save the image
     ImageSave(image, fileName);
+
+    // also save a DFT of the image
+    if (c_doDFT)
+    {
+        SImageDataComplex dftData;
+        ImageDFT(image, dftData);
+
+        SImageData DFTMagImage;
+        GetMagnitudeData(dftData, DFTMagImage);
+
+        char buffer[256];
+        sprintf(buffer, "%s.mag.bmp", fileName);
+
+        ImageSave(DFTMagImage, buffer);
+    }
+}
+
+//======================================================================================
+void NormalizeHistogram (SImageDataFloat& image)
+{
+    struct SHistogramHelper
+    {
+        float value;
+        size_t pixelIndex;
+    };
+    static std::vector<SHistogramHelper> pixels;
+    pixels.resize(image.m_width * image.m_height);
+
+    // put all the pixels into the array
+    for (size_t i = 0, c = image.m_width * image.m_height; i < c; ++i)
+    {
+        pixels[i].value = image.m_pixels[i];
+        pixels[i].pixelIndex = i;
+    }
+
+    // TODO: is the shuffle needed? is there ever really a duplicate? do it even if not, but note the outcome in the blog.
+    // shuffle the pixels to randomly order ties
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    std::shuffle(pixels.begin(), pixels.end(), rng);
+
+    // sort the pixels by value
+    std::sort(
+        pixels.begin(),
+        pixels.end(),
+        [] (const SHistogramHelper& a, const SHistogramHelper& b)
+        {
+            return a.value < b.value;
+        }
+    );
+
+    // use the pixel's place in the array as the new value, and write it back to the image
+    for (size_t i = 0, c = image.m_width * image.m_height; i < c; ++i)
+    {
+        float value = float(i) / float(c - 1);
+        image.m_pixels[pixels[i].pixelIndex] = value;
+    }
 }
 
 //======================================================================================
 int main (int argc, char ** argv)
 {
-    const size_t c_imageSize = 256;
-
     // seed the random number generator
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_real_distribution<float> dist(0.0f, 1.0f);
 
+    // calculate the blur size from our sigma
+    int blurSize = PixelsNeededForSigma(c_blurSigma) | 1;
+
     // blue noise
     {
-        std::array<float, c_imageSize*c_imageSize> noise;
-
-        for (float& v : noise)
+        // generate some white noise
+        SImageDataFloat noise;
+        ImageFloatInit(noise, c_imageSize, c_imageSize);
+        for (float& v : noise.m_pixels)
         {
             v = dist(rng);
         }
 
-        SaveFloatArrayAsBMP(noise, "noise.bmp", c_imageSize);
-        
-        // TODO: iteratively blur, subtract, rescale, save
+        // save off the starting white noise
+        const char* baseFileName = "bluenoise%zu.bmp";
+        char fileName[256];
+
+        sprintf(fileName, baseFileName, 0);
+        SaveImageFloatAsBMP(noise, fileName);
+
+        // iteratively high pass filter and rescale histogram to the 0 to 1 range
+        SImageDataFloat blurredImage;
+        for (size_t blurIndex = 0; blurIndex < c_numBlurs; ++blurIndex)
+        {
+            // get a low passed version of the current image
+            ImageGaussianBlur(noise, blurredImage, c_blurSigma, c_blurSigma, blurSize, blurSize);
+
+            // subtract the low passed version to get the high passed version
+            for (size_t pixelIndex = 0; pixelIndex < c_imageSize * c_imageSize; ++pixelIndex)
+                noise.m_pixels[pixelIndex] -= blurredImage.m_pixels[pixelIndex];
+
+            // put all pixels between 0.0 and 1.0 again
+            NormalizeHistogram(noise);
+
+            // save this image
+            sprintf(fileName, baseFileName, blurIndex + 1);
+            SaveImageFloatAsBMP(noise, fileName);
+        }
     }
+
+    // TODO: make red noise work after blue noise is working
+    /*
+    // red noise
+    {
+        // generate some white noise
+        SImageDataFloat noise;
+        ImageFloatInit(noise, c_imageSize, c_imageSize);
+        for (float& v : noise.m_pixels)
+        {
+            v = dist(rng);
+        }
+
+        // save off the starting white noise
+        const char* baseFileName = "rednoise%zu.bmp";
+        char fileName[256];
+
+        sprintf(fileName, baseFileName, 0);
+        SaveImageFloatAsBMP(noise, fileName, false);
+
+        // iteratively high pass filter and rescale histogram to the 0 to 1 range
+        SImageDataFloat blurredImage;
+        for (size_t blurIndex = 0; blurIndex < c_numBlurs; ++blurIndex)
+        {
+            // get a low passed version of the current image
+            ImageGaussianBlur(noise, blurredImage, c_blurSigma, c_blurSigma, blurSize, blurSize);
+
+            // TODO: set noise to the low passed version
+
+            // TODO: need to re-adjust histogram!
+
+            // save this image
+            sprintf(fileName, baseFileName, blurIndex + 1);
+            SaveImageFloatAsBMP(noise, fileName, blurIndex == c_numBlurs - 1);
+        }
+    }
+    */
 
     return 0;
 }
@@ -192,12 +618,8 @@ int main (int argc, char ** argv)
 
 TODO:
 
-* blue noise: 
- * generate white noise
- * blur it, subtract blur from white noise, make it be full range again.
- * to rehape it, make a struct of: value, x, y.  shuffle it. sort by value. replace value by percentage in list.
- * i think blur maybe needs to wrap around. maybe have blur work in integer coordinates?
- * show dft magnitude too!
+* show how different blur sigmas affect the frequencies and the final noise output
+* get red noise working
 
 * different blurs are different quality low pass filters.
  * box blur = low quality
@@ -206,23 +628,27 @@ TODO:
  * maybe need to do a post on that next? or getting lost in the weeds...?
  * not sure if worth white, but could show box vs gaussian?
 
-* if things are slow, can you thread it at all?
-
-* do similar for red noise.
-
 * animated gifs showing it evolve?
 
 * maybe also animate blue noise w/ golden ratio, and mention that in this post? vs a flip book of blue noise?
 
-* note: timothy lottes says to use sort that's more efficient for fixed sizes keys (radix sort), and fits it in 64 bits instead of a struct.
+? should we DFT the float array? probably would be good if we can! (higher quality results)
 
-* note: you can't make blue noise by making white noise, doing DFT, modifying stuff, then doing IDFT. that is filtering it and is equivelant to what you are doing here.
+* TODOs in code
 
 ? how to demo the quality of this noise?
+ * compare image and DFT to blue noise image and DFT
+ * show thresholding (animation?) of blue noise texture
+ * some kind of actual sampling thing or something?
 
 * links
  * https://bartwronski.com/2016/10/30/dithering-part-two-golden-ratio-sequence-blue-noise-and-highpass-and-remap/
  * https://gpuopen.com/vdr-follow-up-fine-art-of-film-grain/
  * gaussian blur: https://blog.demofox.org/2015/08/19/gaussian-blur/
+
+Blog:
+* note that blur needs to wrap around!
+* note: you can't make blue noise by making white noise, doing DFT, modifying stuff, then doing IDFT. that is filtering it and is equivelant to what you are doing here.
+* note: timothy lottes says to use sort that's more efficient for fixed sizes keys (radix sort), and fits it in 64 bits instead of a struct.
 
 */
